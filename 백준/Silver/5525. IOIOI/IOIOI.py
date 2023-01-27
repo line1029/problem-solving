e25bcf2bcdbc575fa20a2e1932da05435bd8a0f8
line1029 <95160680+line1029@users.noreply.github.com>
2023-01-27 11:32:35 +0900

    [Silver I] Title: IOIOI, Time: 52 ms, Memory: 32436 KB -BaekjoonHub

---
+++ 백준/Silver/5525. IOIOI/IOIOI.py
@@ -0,0 +1,15 @@
+from sys import stdin
+n = int(stdin.readline())
+m = int(stdin.readline())
+ioi = stdin.readline().rstrip().replace("II", "IOOI").split("OO")
+leng = 2*n + 1
+cnt = 0
+for s in ioi:
+    k = len(s)
+    if not k:
+        continue
+    k -= (s[0] == "O") + (s[-1] == "O")
+    if k - leng < 0:
+        continue
+    cnt += (k - leng)//2 + 1
+print(cnt)
