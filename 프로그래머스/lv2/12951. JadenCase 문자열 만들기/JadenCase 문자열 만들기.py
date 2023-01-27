6bd4947edecff43c8cec9c67b1eafdb627f9083a
line1029 <95160680+line1029@users.noreply.github.com>
2023-01-27 10:24:34 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.py
+++ 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.py
@@ -1,4 +1,3 @@
 def solution(s):
-    print(s.split(" "))
-    arr = list(map(lambda x: x[0].upper() + x[1:].lower() if x else x, s.split(" ")))
+    arr = map(lambda x: x[0].upper() + x[1:].lower() if x else x, s.split(" "))
     return " ".join(arr)
