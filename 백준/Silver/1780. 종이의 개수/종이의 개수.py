6321870d7aec49cdd54f0d99ada8898e861ead61
line1029 <95160680+line1029@users.noreply.github.com>
2023-01-27 11:57:57 +0900

    [Silver II] Title: 종이의 개수, Time: 1728 ms, Memory: 221932 KB -BaekjoonHub

--- 백준/Silver/1780. 종이의 개수/종이의 개수.py
+++ 백준/Silver/1780. 종이의 개수/종이의 개수.py
@@ -4,25 +4,21 @@ n = int(stdin.readline())
 mat = []
 for _ in range(n):
     mat.append(list(map(int, stdin.readline().split())))
+ans = [0, 0, 0]
 def rec(n, mat):
-    c = Counter()
-    for row in mat:
-        c += Counter(row)
-    ans = [0, 0, 0]
-    if len(c) == 1:
-        ans[0] += -1 in c
-        ans[1] += 0 in c
-        ans[2] += 1 in c
-        return ans
-    else:
-        n_nex = n//3
-        for i in range(9):
-            r, c = i//3, i%3
-            mat_nex = [mat[i][c*n_nex:(c+1)*n_nex] for i in range(r*n_nex, (r+1)*n_nex)]
-            ans_i = rec(n_nex, mat_nex)
-            ans[0] += ans_i[0]
-            ans[1] += ans_i[1]
-            ans[2] += ans_i[2]
-        return ans
-            
-print("\n".join(map(str, rec(n, mat))))
+    if n == 1:
+        ans[mat[0][0]] += 1
+        return
+    standard = mat[0][0]
+    for i in range(n):
+        for j in range(n):
+            if mat[i][j] != standard:
+                for k in range(3):
+                    for l in range(3):
+                        rec(n//3, [row[l*n//3:(l+1)*n//3] for row in mat[k*n//3:(k+1)*n//3]])
+                return
+    ans[standard] += 1
+    return
+rec(n, mat)
+ans = ans[-1:] + ans[:-1]
+print("\n".join(map(str, ans)))
