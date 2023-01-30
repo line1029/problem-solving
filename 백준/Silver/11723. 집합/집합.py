a71d316b02502b935cb52174e3a8fc8d082d4dc0
line1029 <95160680+line1029@users.noreply.github.com>
2023-01-30 11:40:55 +0900

    [Silver V] Title: 집합, Time: 1076 ms, Memory: 121872 KB -BaekjoonHub

--- 백준/Silver/11723. 집합/집합.py
+++ 백준/Silver/11723. 집합/집합.py
@@ -1,11 +1,10 @@
 from sys import stdin, stdout
 m = int(stdin.readline())
 _set = 0
-_all = 0b11111111111111111111
 for _ in range(m):
     order = stdin.readline().split()
     if order[0] == "all":
-        _set = _all
+        _set = (1 << 20) - 1
     elif order[0] == "empty":
         _set = 0
     else:
@@ -15,6 +14,9 @@ for _ in range(m):
         elif order[0] == "remove":
             _set &= ~(1 << num)
         elif order[0] == "check":
-            print((_set & (1 << num)) >> num)
+            if _set & (1 << num):
+                stdout.write("1\n")
+            else:
+                stdout.write("0\n")
         else:
             _set ^= (1 << num)
