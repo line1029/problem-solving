c9f71380178208d2e8b8a400b4ebcb08f0eae971
line1029 <95160680+line1029@users.noreply.github.com>
2023-01-30 12:06:47 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 332 ms, Memory: 171476 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
@@ -1,23 +1,24 @@
 from sys import stdin
-from collections import defaultdict
 n, m = map(int, stdin.readline().split())
 edges = map(lambda x: tuple(map(int, x.split())), stdin.read().splitlines())
-graph = defaultdict(list)
+parent = list(range(n+1))
+rank = [0]*(n+1)
+def _union(x, y):
+    x, y = _find(x), _find(y)
+    if x == y:
+        return
+    if rank[x] < rank[y]:
+        x, y = y, x
+    if rank[x] == rank[y]:
+        rank[x] += 1
+    parent[y] = x
+def _find(x):
+    if x != parent[x]:
+        parent[x] = _find(parent[x])
+    return parent[x]
 for n1, n2 in edges:
-    graph[n1].append(n2)
-    graph[n2].append(n1)
-visited = [0]*n
-cnt = 0
-for node in range(1, n+1):
-    if not visited[node - 1]:
-        cnt += 1
-        visited[node - 1] = 1
-        stack = [node]
-        while stack:
-            cur = stack.pop()
-            for nex in graph[cur]:
-                if not visited[nex - 1]:
-                    visited[nex - 1] = 1
-                    stack.append(nex)
-            
-print(cnt)
+    _union(n1, n2)
+
+for i in range(1, n+1):
+    _find(i)
+print(len(set(parent)) - 1)
