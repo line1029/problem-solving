3cb7d5b7a7f7f0a93c4d9c0d6834546c0aa1405d
line1029 <95160680+line1029@users.noreply.github.com>
2023-02-02 01:31:33 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 3620 ms, Memory: 136004 KB -BaekjoonHub

--- 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.py
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.py
@@ -1,10 +1,8 @@
 from sys import stdin, stdout
 n = int(stdin.readline())
 arr = [0]*10001
-for i in range(n):
-    num = int(stdin.readline())
-    arr[num] += 1
-for i in range(1, 10001):
-    if arr[i]:
-        for j in range(arr[i]):
-            stdout.write(f'{i}\n')
+for _ in range(n):
+    arr[int(stdin.readline())] += 1
+for i in range(10001):
+    for j in range(arr[i]):
+        stdout.write(f'{i}\n')
