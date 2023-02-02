b8186fde8924bf02a2a0cbee24e00afad77a4064
line1029 <95160680+line1029@users.noreply.github.com>
2023-02-02 12:38:30 +0900

    [Gold II] Title: 트리의 지름, Time: 560 ms, Memory: 156004 KB -BaekjoonHub

---
+++ 백준/Gold/1167. 트리의 지름/트리의 지름.py
@@ -0,0 +1,25 @@
+from sys import stdin
+n = int(stdin.readline())
+graph = [None]*(n+1)
+visited = [0]*(n+1)
+ans = [0, 1]
+for _ in range(n):
+    v, *edges = map(int, stdin.readline().split())
+    graph[v] = [(edges[2*i], edges[2*i + 1]) for i in range(len(edges)//2)]
+def dfs(dist, node):
+    cnt = 0
+    for v, w in graph[node]:
+        if not visited[v]:
+            visited[v] = 1
+            dfs(dist + w, v)
+            visited[v] = 0
+            cnt += 1
+    if not cnt and ans[0] < dist:
+        ans[0] = dist
+        ans[1] = node
+visited[1] = 1
+dfs(0, 1)
+visited[1] = 0
+visited[ans[1]] = 1
+dfs(0, ans[1])
+print(ans[0])
