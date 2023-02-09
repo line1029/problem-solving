096bdea1dab7f9494aa49da1361d0246258329d4
line1029 <95160680+line1029@users.noreply.github.com>
2023-02-09 20:39:13 +0900

    [Gold IV] Title: 좋은 친구, Time: 208 ms, Memory: 64636 KB -BaekjoonHub

--- 백준/Gold/3078. 좋은 친구/좋은 친구.py
+++ 백준/Gold/3078. 좋은 친구/좋은 친구.py
@@ -1,9 +1,9 @@
-from collections import Counter
-from itertools import islice
 from sys import stdin
 n, k = map(int, stdin.readline().split())
 students = list(map(len, stdin.read().splitlines()))
-good_friends = Counter(islice(students, k + 1))
+good_friends = dict([i, 0] for i in range(2, 21))
+for i in range(k + 1):
+    good_friends[students[i]] += 1
 ans = 0
 for i in range(n - k - 1):
     good_friends[students[i]] -= 1
