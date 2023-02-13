d73699f7caf8c1c2cbbee99e08d62e3e8ad6d83f
line1029 <95160680+line1029@users.noreply.github.com>
2023-02-14 00:35:46 +0900

    [Bronze III] Title: 네 번째 점, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/3009. 네 번째 점/네 번째 점.py
+++ 백준/Bronze/3009. 네 번째 점/네 번째 점.py
@@ -1,14 +1,12 @@
 from sys import stdin
-from collections import defaultdict
-x_dict, y_dict = defaultdict(int), defaultdict(int)
+a, b = [], []
 for x, y in map(lambda x: map(int, x.split()), stdin.read().splitlines()):
-    x_dict[x] += 1
-    y_dict[y] += 1
-ans = [0, 0]
-for x in x_dict:
-    if x_dict[x] == 1:
-        ans[0] = x
-for y in y_dict:
-    if y_dict[y] == 1:
-        ans[1] = y
-print(" ".join(map(str, ans)))
+    if x not in a:
+        a.append(x)
+    else:
+        a.remove(x)
+    if y not in b:
+        b.append(y)
+    else:
+        b.remove(y)
+print(" ".join(map(str, a + b)))
