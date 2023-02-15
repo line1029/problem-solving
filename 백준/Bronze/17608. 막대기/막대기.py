43e1732b8fccf766d35044d04306e91d066bca53
line1029 <95160680+line1029@users.noreply.github.com>
2023-02-16 00:54:19 +0900

    [Bronze II] Title: 막대기, Time: 72 ms, Memory: 39004 KB -BaekjoonHub

--- 백준/Bronze/17608. 막대기/막대기.py
+++ 백준/Bronze/17608. 막대기/막대기.py
@@ -2,7 +2,7 @@ from sys import stdin
 stdin.readline()
 last = 0
 cnt = 0
-for num in map(int, stdin.read().splitlines()[::-1]):
+for num in map(int, reversed(stdin.read().splitlines())):
     if num > last:
         cnt += 1
         last = num
