4d4a8a629681c824a056e0eaacaccdfb97521d88
line1029 <95160680+line1029@users.noreply.github.com>
2023-02-16 11:24:47 +0900

    [level 2] Title: k진수에서 소수 개수 구하기, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.py
+++ 프로그래머스/lv2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.py
@@ -5,38 +5,52 @@ def solution(n, k):
     while n:
         n_base_k.append(n%k)
         n //= k
-    n_base_k = "".join(map(str, n_base_k[::-1])).split("0")
-    n_base_k = [int(i) for i in n_base_k if i]
-    if not n_base_k:
-        return 0
-    n_base_k.sort()
-    # n_base_k2 = []
-    # while n_base_k and n_base_k[-1] > 10000:
-    #     n_base_k2.append(n_base_k.pop())
-    def check_prime_byarr(n_base_k):
-        max_candidate = n_base_k[-1]
-        if max_candidate == 1:
-            return 0
-        prime_arr = [False, True]*(max_candidate//2 + 1)
-        prime_arr[1] = False
-        prime_arr[2] = True
-        for i in range(3, int(max_candidate**(0.5)) + 1, 2):
-            prime_arr[i*i::2*i] = [False]*len(prime_arr[i*i::2*i])
-        ans = [prime_arr[i] for i in n_base_k]
-        if ans:
-            return sum(ans)
+    candidates = "".join(map(str, reversed(n_base_k))).split("0")
+    candidates = [int(i) for i in candidates if i]
+    if not candidates:
         return 0
     
-    def check_prime_bydiv(num):
-        if num == 1:
-            return 0
-        if num == 2:
-            return 1
-        if num%2 == 0:
-            return 0
-        for i in range(3, int(num**(0.5)) + 1, 2):
-            if num%i == 0:
-                return 0
-        return 1
+    def power(a, b, mod):
+        res = 1
+        while b:
+            if b&1:
+                res = (res * a) % mod
+            b >>= 1
+            a = (a * a) % mod
+        return res
+
+
+    def miller_rabin(n, a):
+        if n % a == 0:
+            return True
+
+        k = n - 1
+        tmp = power(a, k, n)
+        if tmp != 1:
+            return False
+        while not k&1:
+            k >>= 1
+            if tmp == n - 1:
+                return True
+            elif tmp != 1:
+                return False
+        return tmp == 1 or tmp == n - 1
+
+
+    def is_prime(n):
+        if n <= 1:
+            return False
+        if any([n == 2, n == 3, n == 5, n == 7]):
+            return True
+        if any([n % 2 == 0, n % 3 == 0, n % 5 == 0, n % 7 == 0]):
+            return False
+        if n < 1 << 32:
+            base = [2, 7, 61]
+        else:
+            base = [2, 325, 9375, 28178, 450775, 9780504, 1795265022]
+        for a in base:
+            if not miller_rabin(n, a):
+                return False
+        return True
     
-    return sum(check_prime_bydiv(i) for i in n_base_k)
+    return sum(is_prime(i) for i in candidates)
