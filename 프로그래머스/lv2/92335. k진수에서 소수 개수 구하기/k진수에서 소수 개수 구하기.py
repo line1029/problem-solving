28642f78b4e63c343d5a8fc524ddfde9025ab3fb
line1029 <95160680+line1029@users.noreply.github.com>
2023-02-16 15:19:38 +0900

    [level 2] Title: k진수에서 소수 개수 구하기, Time: 0.03 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/lv2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.py
+++ 프로그래머스/lv2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.py
@@ -3,37 +3,25 @@ def solution(n, k):
         return 0
     n_base_k = []
     while n:
-        n_base_k.append(n%k)
+        n_base_k.append(n % k)
         n //= k
     candidates = "".join(map(str, reversed(n_base_k))).split("0")
     candidates = [int(i) for i in candidates if i]
     if not candidates:
         return 0
-    
-    def power(a, b, mod):
-        res = 1
-        while b:
-            if b&1:
-                res = (res * a) % mod
-            b >>= 1
-            a = (a * a) % mod
-        return res
-
 
     def miller_rabin(n, a):
         if n % a == 0:
             return True
 
         k = n - 1
-        tmp = power(a, k, n)
-        if tmp != 1:
-            return False
-        while not k&1:
-            k >>= 1
+        while not k & 1:
+            tmp = pow(a, k, n)
             if tmp == n - 1:
                 return True
             elif tmp != 1:
                 return False
+            k >>= 1
         return tmp == 1 or tmp == n - 1
 
 
@@ -44,10 +32,7 @@ def solution(n, k):
             return True
         if any([n % 2 == 0, n % 3 == 0, n % 5 == 0, n % 7 == 0]):
             return False
-        if n < 1 << 32:
-            base = [2, 7, 61]
-        else:
-            base = [2, 325, 9375, 28178, 450775, 9780504, 1795265022]
+        base = [2, 7, 61]
         for a in base:
             if not miller_rabin(n, a):
                 return False
