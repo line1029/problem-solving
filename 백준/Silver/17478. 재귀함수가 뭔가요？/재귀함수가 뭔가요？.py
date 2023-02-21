70a5b7b8468e711aa9ac80d97fec935c8f84e32b
line1029 <95160680+line1029@users.noreply.github.com>
2023-02-21 16:48:06 +0900

    [Silver V] Title: 재귀함수가 뭔가요?, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/17478. 재귀함수가 뭔가요？/재귀함수가 뭔가요？.py
+++ 백준/Silver/17478. 재귀함수가 뭔가요？/재귀함수가 뭔가요？.py
@@ -1,18 +1,14 @@
+# iterative sol
 s = ['"재귀함수가 뭔가요?"',
      '"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어.',
      '마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지.',
      '그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어."']
 a = '라고 답변하였지.'
 d = "____"
-
-def recursive_humor(n, k):
-    if n == 0:
-        print(d*k, s[0], "\n", d*k, '"재귀함수는 자기 자신을 호출하는 함수라네"', "\n", d*k, a, sep="")
-        return
-    print(*map(lambda x: d*k + x, s), sep="\n")
-    recursive_humor(n - 1, k + 1)
-    print(d*k, a, sep="")
-
-
 print("어느 한 컴퓨터공학과 학생이 유명한 교수님을 찾아가 물었다.")
-recursive_humor(int(input()), 0)
+n = int(input())
+for i in range(n):
+    print(*map(lambda x: d*i + x, s), sep="\n")
+print(d*n, s[0], "\n", d*n, '"재귀함수는 자기 자신을 호출하는 함수라네"', "\n", d*n, a, sep="")
+for i in range(n-1, -1, -1):
+    print(d*i, a, sep="")
