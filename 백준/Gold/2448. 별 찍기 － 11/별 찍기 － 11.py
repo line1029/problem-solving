87d49bafa61528564f47c71e50ee4413d5a0dfdd
line1029 <95160680+line1029@users.noreply.github.com>
2023-02-21 17:54:16 +0900

    [Gold IV] Title: 별 찍기 - 11, Time: 100 ms, Memory: 90752 KB -BaekjoonHub

--- 백준/Gold/2448. 별 찍기 － 11/별 찍기 － 11.py
+++ 백준/Gold/2448. 별 찍기 － 11/별 찍기 － 11.py
@@ -7,7 +7,7 @@ while not n & 1:
     po += 1
     n //= 2
 for i in range(po):
-    p1 = ("".join([" "*(3*2**i), x, " "*(3*2**i)]) for x in pattern)
+    p1 = (" "*(3*2**i) + x + " "*(3*2**i) for x in pattern)
     p2 = (" ".join(x) for x in zip(pattern, pattern))
     pattern = [*p1, *p2]
-stdout.writelines(f"{p}\n" for p in pattern)
+print("\n".join(pattern))
