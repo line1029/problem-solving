981a92c4d53d05779b80b77a88b34a0406d30629
line1029 <95160680+line1029@users.noreply.github.com>
2023-03-27 17:26:32 +0900

    [Silver II] Title: 쇠막대기, Time: 68 ms, Memory: 31388 KB -BaekjoonHub

--- 백준/Silver/10799. 쇠막대기/쇠막대기.py
+++ 백준/Silver/10799. 쇠막대기/쇠막대기.py
@@ -1,9 +1,8 @@
 from sys import stdin
-from itertools import islice
 s = stdin.readline()
 ans = 0
-stack = 1
-for idx, char in enumerate(islice(s, 1, len(s) - 1), 1):
+stack = 0
+for idx, char in enumerate(s):
     if char == ")":
         stack -= 1
         if s[idx - 1] == "(":
