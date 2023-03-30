9a4e3b33a732ebf7a1664439d18c04cd68354a51
line1029 <95160680+line1029@users.noreply.github.com>
2023-03-30 19:51:18 +0900

    [Silver IV] Title: 카드, Time: 136 ms, Memory: 56960 KB -BaekjoonHub

--- 백준/Silver/11652. 카드/카드.py
+++ 백준/Silver/11652. 카드/카드.py
@@ -2,4 +2,4 @@ from sys import stdin
 from collections import Counter
 stdin.readline()
 c = Counter(map(int, stdin.read().splitlines()))
-print(sorted(c.most_common(), key=lambda x: (-x[1], x[0]))[0][0])
+print(sorted(c.items(), key=lambda x: (-x[1], x[0]))[0][0])
