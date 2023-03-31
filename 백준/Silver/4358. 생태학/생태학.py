e98b8b3c06332c4a7160e97cfb3205503242ba15
line1029 <95160680+line1029@users.noreply.github.com>
2023-03-31 10:30:52 +0900

    [Silver II] Title: 생태학, Time: 256 ms, Memory: 118828 KB -BaekjoonHub

---
+++ 백준/Silver/4358. 생태학/생태학.py
@@ -0,0 +1,7 @@
+from sys import stdin, stdout
+from collections import Counter
+trees = stdin.read().splitlines()
+n = len(trees)
+trees = Counter(trees)
+trees = [f"{i} {j/n * 100:.4f}" for i, j in sorted(trees.items())]
+stdout.write("\n".join(trees))
