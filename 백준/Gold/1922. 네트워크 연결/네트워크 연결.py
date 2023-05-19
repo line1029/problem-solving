2b1d347b110d92340ad6a7fa003f127d261a86da
line1029 <95160680+line1029@users.noreply.github.com>
2023-05-19 14:32:20 +0900

    [Gold IV] Title: 네트워크 연결, Time: 280 ms, Memory: 62084 KB -BaekjoonHub

--- 백준/Gold/1922. 네트워크 연결/네트워크 연결.py
+++ 백준/Gold/1922. 네트워크 연결/네트워크 연결.py
@@ -1,37 +1,73 @@
-# kruskal with union-find
+# improved prim
 from sys import stdin
 import heapq
 n = int(stdin.readline())
 m = int(stdin.readline())
-edges = list(map(lambda x: list(map(int, x.split()))[::-1], stdin.read().splitlines()))
-heapq.heapify(edges)
-
-parent = list(range(n+1))
-rank = [0]*(n+1)
-# using path-compression
-def _find(x):
-    if parent[x] != x:
-        parent[x] = _find(parent[x])
-        return parent[x]
-    return x
-
-# union by rank
-def _union(x, y):
-    x, y = _find(x), _find(y)
-    if x == y:
-        return False
-    
-    if rank[x] < rank[y]:
-        x, y = y, x
-    if rank[x] == rank[y]:
-        rank[x] += 1
-    parent[y] = x
-    return True
+graph = [list() for _ in range(n + 1)]
+for a, b, c in map(lambda x: map(int, x.split()), stdin.read().splitlines()):
+    if a == b:
+        continue
+    graph[a].append((c, b))
+    graph[b].append((c, a))
 
 
+# implement binary heap with decrease key logic
+def decrease_key(heap, node_idx):
+    while node_idx:
+        parent_idx = (node_idx - 1) >> 1
+        if heap[parent_idx] < heap[node_idx]:
+            break
+        swap(heap, node_idx, parent_idx)
+        node_idx = parent_idx
+
+
+def swap(heap, i, j):
+    heap[i], heap[j] = heap[j], heap[i]
+    heap[i][2] = i
+    heap[j][2] = j
+
+
+def sift_down(heap, i):
+    n = len(heap)
+    while True:
+        lo = i
+        l = (i << 1) + 1
+        r = (i + 1) << 1
+        if l < n and heap[l][0] < heap[i][0]:
+            lo = l
+        if r < n and heap[r][0] < heap[lo][0]:
+            lo = r
+        
+        if lo == i:
+            break
+
+        swap(heap, lo, i)
+        i = lo
+
+
+def extract_min(heap):
+    last_elt = heap.pop()
+    if heap:
+        res = heap[0]
+        heap[0] = last_elt
+        last_elt[2] = 0
+        sift_down(heap, 0)
+        return res
+    return last_elt
+
+
+pq = [[10001, i + 1, i] for i in range(n)]
+v_dict = {i+1:pq[i] for i in range(n)}
+pq[0][0] = 0
 ans = 0
-while edges:
-    c, b, a = heapq.heappop(edges)
-    if _union(a, b):
-        ans += c
+while pq:
+    weight, node, _ = extract_min(pq)
+    ans += weight
+    v_dict.pop(node)
+    for edge in graph[node]:
+        if edge[1] in v_dict:
+            if v_dict[edge[1]][0] > edge[0]:
+                v_dict[edge[1]][0] = edge[0]
+                decrease_key(pq, v_dict[edge[1]][2])
+
 print(ans)
