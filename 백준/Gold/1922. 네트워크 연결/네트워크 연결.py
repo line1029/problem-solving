d9c966941500607a83e4cc56e303cad038d876c9
line1029 <95160680+line1029@users.noreply.github.com>
2023-05-19 12:51:58 +0900

    [Gold IV] Title: 네트워크 연결, Time: 512 ms, Memory: 58608 KB -BaekjoonHub

---
+++ 백준/Gold/1922. 네트워크 연결/네트워크 연결.py
@@ -0,0 +1,41 @@
+# kruskal with union-find
+from sys import stdin
+import heapq
+from collections import defaultdict
+n = int(stdin.readline())
+m = int(stdin.readline())
+edges = list(map(lambda x: list(map(int, x.split()))[::-1], stdin.read().splitlines()))
+heapq.heapify(edges)
+
+parent = dict()
+rank = defaultdict(int)
+# using path-splitting
+def _find(x):
+    while parent[x] != x:
+        x, parent[x] = parent[x], parent[parent[x]]
+    return x
+
+
+def _union(x, y):
+    x, y = _find(x), _find(y)
+    if x == y:
+        return False
+    
+    if rank[x] < rank[y]:
+        x, y = y, x
+    if rank[x] == rank[y]:
+        rank[x] += 1
+    parent[y] = x
+    return True
+
+
+ans = 0
+while edges:
+    c, b, a = heapq.heappop(edges)
+    if b not in parent:
+        parent[b] = b
+    if a not in parent:
+        parent[a] = a
+    if _union(a, b):
+        ans += c
+print(ans)
