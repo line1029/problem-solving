00cd63a311cc98775fdfa23201147b1c5cde3291
line1029 <95160680+line1029@users.noreply.github.com>
2023-05-25 18:34:43 +0900

    [Gold I] Title: 트리의 순회, Time: 224 ms, Memory: 59452 KB -BaekjoonHub

--- 백준/Gold/2263. 트리의 순회/트리의 순회.py
+++ 백준/Gold/2263. 트리의 순회/트리의 순회.py
@@ -1,23 +1,21 @@
-from sys import stdin, stdout, setrecursionlimit
+from sys import stdin, stdout
 n = int(stdin.readline())
 inorder = list(map(int, stdin.readline().split()))
 inorder_index = {val:idx for idx, val in enumerate(inorder)}
 postorder = list(map(int, stdin.readline().split()))
-if n > 999:
-    setrecursionlimit(n + 1)
 
 preorder = []
-def get_preorder(in_start, in_end, post_start, post_end):
+stack = [(0, n-1, 0, n-1)]
+while stack:
+    in_start, in_end, post_start, post_end = stack.pop()
     if in_start > in_end or post_start > post_end:
-        return
+        continue
     root = postorder[post_end]
     preorder.append(root)
-    if in_start == in_end:
-        return
     in_root_idx = inorder_index[root]
     left_length = in_root_idx - in_start
-    get_preorder(in_start, in_start + left_length - 1, post_start, post_start + left_length - 1)
-    get_preorder(in_start + left_length + 1, in_end, post_start + left_length, post_end - 1)
+    stack.append((in_start + left_length + 1, in_end, post_start + left_length, post_end - 1))
+    stack.append((in_start, in_start + left_length - 1, post_start, post_start + left_length - 1))
 
-get_preorder(0, n-1, 0, n-1)
+
 stdout.write(" ".join(map(str, preorder)))
