ef5e09a51d3e39ccee07ae4c5c65ac1b812dfd36
line1029 <95160680+line1029@users.noreply.github.com>
2023-05-31 02:11:44 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 52 ms, Memory: 33376 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.py
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.py
@@ -1,14 +1,29 @@
-# dp with O(n^2)
 from sys import stdin
-from itertools import islice
-n = int(stdin.readline())
-seq = list(map(int, stdin.readline().split()))
-dp = [1] * n
-total_max = 1
-for cur_idx, cur_num in enumerate(seq):
-    for prev_idx, prev_lis in enumerate(islice(dp, cur_idx)):
-        if cur_num > seq[prev_idx] and dp[cur_idx] < prev_lis + 1:
-            dp[cur_idx] = prev_lis + 1
-            total_max = max(prev_lis + 1, total_max)
+from math import ceil, log2
+def update(tree, tree_idx, start, end, seq_idx, max_val):
+    if seq_idx < start or seq_idx > end:
+        return
+    if start == end == seq_idx:
+        tree[tree_idx] = max_val
+        return
+    update(tree, tree_idx*2, start, (start+end)//2, seq_idx, max_val)
+    update(tree, tree_idx*2+1, (start+end)//2+1, end, seq_idx, max_val)
+    tree[tree_idx] = max(tree[tree_idx*2], tree[tree_idx*2+1])
 
-print(total_max)
+def query(tree, tree_idx, start, end, left, right):
+    if start > right or end < left:
+        return 0
+    if left <= start and end <= right:
+        return tree[tree_idx]
+    lmax = query(tree, tree_idx*2, start, (start+end)//2, left, right)
+    rmax = query(tree, tree_idx*2+1, (start+end)//2+1, end, left, right)
+    return max(lmax, rmax)
+n = int(stdin.readline())
+seq = map(int, stdin.readline().split())
+h = ceil(log2(n)) + 1
+tree_size = 1 << h
+segment_tree = [0] * tree_size
+for seq_idx, seq_val in sorted(enumerate(seq), key=lambda x: (x[1], -x[0])):
+    max_lis = query(segment_tree, 1, 0, n-1, 0, seq_idx) + 1
+    update(segment_tree, 1, 0, n-1, seq_idx, max_lis)
+print(segment_tree[1])
