4f4ff4949f93b5fd985558c7c64d144f44b8849c
line1029 <95160680+line1029@users.noreply.github.com>
2023-06-01 14:45:14 +0900

    [Platinum V] Title: 멋진 부분집합, Time: 240 ms, Memory: 116624 KB -BaekjoonHub

--- 백준/Platinum/27470. 멋진 부분집합/멋진 부분집합.py
+++ 백준/Platinum/27470. 멋진 부분집합/멋진 부분집합.py
@@ -1,81 +1,29 @@
-from sys import stdin, setrecursionlimit
+from sys import stdin
 from collections import Counter
-from math import gcd
-from random import randint
-setrecursionlimit(1000000)
-
-def power(a, b, mod):
-    res = 1
-    while b:
-        if b&1:
-            res = (res * a) % mod
-        b >>= 1
-        a = (a * a) % mod
-    return res
-
-
-def miller_rabin(n, a):
-    if n <= 1:
-        return False
-    if n == 2:
-        return True
-    if n == a:
-        return True
-    
-    k = n - 1
-    while True:
-        tmp = power(a, k, n)
-        if tmp == n - 1:
-            return True
-        if k&1:
-            return tmp == 1 or tmp == n - 1
-        k >>= 1
-
-
-def is_prime(n):
-    base = [2, 7, 61]
-    for a in base:
-        if not miller_rabin(n, a):
-            return False
-    return True
-
-
-def pollard_rho(n):
-    if is_prime(n):
-        return n
-    if n == 1:
-        return 1
-    if n % 2 == 0:
-        return 2
-    x = y = randint(2, n - 1)
-    c = randint(1, n - 1)
-    d = 1
-    while d == 1:
-        x = ((x*x)%n + c)%n
-        y = ((y*y)%n + c)%n
-        y = ((y*y)%n + c)%n
-        d = gcd(abs(x - y), n)
-        if d == n:
-            return pollard_rho(n)
-    if is_prime(d):
-        return d
-    else:
-        return pollard_rho(d)
-
-def factors(n):
-    res = set()
-    while n > 1:
-        divisor = pollard_rho(n)
-        res.add(divisor)
-        while n % divisor == 0:
-            n //= divisor
-    return res
-
+def prime_sieve(n):
+    s = [False, True]*int((n//2 + 1)**.5 + 1)
+    s[1] = False
+    s[2] = True
+    for i in range(3, int(n**.5) + 1):
+        if s[i]:
+            s[i*i::i*2] = [False]*(len(s[i*i::i*2]))
+    return [i for i in range(len(s)) if s[i]]
 N = int(stdin.readline())
 nums = list(map(int, stdin.readline().split()))
+n = max(nums)
+fs = prime_sieve(n)
+def factorization(n):
+    res = set()
+    for i in fs:
+        while n%i == 0:
+            res.add(i)
+            n //= i
+    if n != 1:
+        res.add(n)
+    return res
 c = Counter()
 for i in nums:
-    c.update(factors(i))
+    c.update(factorization(i))
 prime, cnt = c.most_common()[0]
 wanted_length = N//2 + N%2
 if cnt < wanted_length:
