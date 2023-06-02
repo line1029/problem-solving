2071936cbd9dde13a4018939a4490da7b8f3eeac
line1029 <95160680+line1029@users.noreply.github.com>
2023-06-02 22:54:23 +0900

    [Gold IV] Title: 미세먼지 안녕!, Time: 752 ms, Memory: 116200 KB -BaekjoonHub

--- 백준/Gold/17144. 미세먼지 안녕！/미세먼지 안녕！.py
+++ 백준/Gold/17144. 미세먼지 안녕！/미세먼지 안녕！.py
@@ -14,38 +14,40 @@ border.update((R, i) for i in range(C))
 border.update(((air_cleaner, 0), (air_cleaner+1, 0)))
 direc = ((-1, 0), (1, 0), (0, 1), (0, -1))
 total_dust = sum(sum(i) for i in grid) + 2
+flag = False
 for _ in range(T):
-    for i, j in product(range(R), range(C)):
-        dust = grid[i][j] // 5
-        if dust <= 0:
-            continue
-        for di, dj in direc:
-            ni, nj = i + di, j + dj
-            if (ni, nj) not in border:
-                subgrid[ni][nj] += dust
-                grid[i][j] -= dust
-    for i, j in product(range(R), range(C)):
-        grid[i][j] += subgrid[i][j]
-        subgrid[i][j] = 0
+    if not flag:
+        flag = True
+        for i, j in product(range(R), range(C)):
+            dust = grid[i][j] // 5
+            if dust <= 0:
+                continue
+            
+            for di, dj in direc:
+                ni, nj = i + di, j + dj
+                if (ni, nj) not in border:
+                    flag = False
+                    subgrid[ni][nj] += dust
+                    grid[i][j] -= dust
+        for i, j in product(range(R), range(C)):
+            grid[i][j] += subgrid[i][j]
+            subgrid[i][j] = 0
+    total_dust -= grid[air_cleaner-1][0] + grid[air_cleaner+2][0]
+    for i in range(air_cleaner - 1, 0, -1):
+        grid[i][0] = grid[i - 1][0]
+    for i in range(air_cleaner + 2, R - 1):
+        grid[i][0] = grid[i + 1][0]
+    for j in range(C - 1):
+        grid[0][j] = grid[0][j + 1]
+        grid[-1][j] = grid[-1][j + 1]
+    for i in range(air_cleaner):
+        grid[i][-1] = grid[i + 1][-1]
+    for i in range(R - 1, air_cleaner + 1, -1):
+        grid[i][-1] = grid[i - 1][-1]
+    for j in range(C - 1, 0, -1):
+        grid[air_cleaner][j] = grid[air_cleaner][j - 1]
+        grid[air_cleaner + 1][j] = grid[air_cleaner + 1][j - 1]
+    grid[air_cleaner][1] = 0
+    grid[air_cleaner + 1][1] = 0
     
-    tmp, grid[air_cleaner][1] = grid[air_cleaner][1], 0
-    for j in range(2, C):
-        tmp, grid[air_cleaner][j] = grid[air_cleaner][j], tmp
-    for i in range(air_cleaner - 1, -1, -1):
-        tmp, grid[i][-1] = grid[i][-1], tmp
-    for j in range(C - 2, -1, -1):
-        tmp, grid[0][j] = grid[0][j], tmp
-    for i in range(1, air_cleaner):
-        tmp, grid[i][0] = grid[i][0], tmp
-    total_dust -= tmp
-    tmp, grid[air_cleaner + 1][1] = grid[air_cleaner + 1][1], 0
-    for j in range(2, C):
-        tmp, grid[air_cleaner + 1][j] = grid[air_cleaner + 1][j], tmp
-    for i in range(air_cleaner + 2, R):
-        tmp, grid[i][-1] = grid[i][-1], tmp
-    for j in range(C - 2, -1, -1):
-        tmp, grid[-1][j] = grid[-1][j], tmp
-    for i in range(R - 2, air_cleaner + 1, -1):
-        tmp, grid[i][0] = grid[i][0], tmp
-    total_dust -= tmp
 print(total_dust)
