da06c8b03a1f71d36a9caec25b074adc5abd9ae2
line1029 <95160680+line1029@users.noreply.github.com>
2023-06-05 12:51:00 +0900

    [Platinum V] Title: 큐빙, Time: 300 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Platinum/5373. 큐빙/큐빙.py
@@ -0,0 +1,47 @@
+from sys import stdin
+from itertools import islice
+SURFACE_INDEX_MAP = {"U":0, "D":1, "F":2, "B":3, "L":4, "R":5}
+ORIENTATION_MAP = {1:[[2, 5, 8], [8, 7, 6], [2, 5, 8], [0, 1, 2]],
+                   0:[[0, 1, 2], [6, 3, 0], [8, 7, 6], [6, 3, 0]]}
+def rotate(cube, surface: str, orientation: str) -> None:
+    surface_idx = SURFACE_INDEX_MAP[surface]
+    surface = cube[surface_idx]
+    side_edge = ORIENTATION_MAP[surface_idx%2]
+    side = [(surface_idx + ((i + 1)//2 + 1)*(-1)**(i + surface_idx%2 + 1))%6 for i in range(4)]
+    if orientation == "-":
+        side = side[::-1]
+        side_edge = side_edge[::-1]
+        for i in range(3//2):
+            for j in range(i, 3 - i - 1):
+                tmp = surface[3*i+j]
+                surface[3*i+j] = surface[3*j+2-i]
+                surface[3*j+2-i] = surface[3*(2-i)+2-j]
+                surface[3*(2-i)+2-j] = surface[3*(2-j)+i]
+                surface[3*(2-j)+i] = tmp
+    else:
+        for i in range(3//2):
+            for j in range(i, 3 - i - 1):
+                tmp = surface[3*i+j]
+                surface[3*i+j] = surface[3*(2-j)+i]
+                surface[3*(2-j)+i] = surface[3*(2-i)+2-j]
+                surface[3*(2-i)+2-j] = surface[3*j+2-i]
+                surface[3*j+2-i] = tmp
+    tmp = [cube[side[3]][i] for i in side_edge[3]]
+    for side_idx, edge_idx in zip(side, side_edge):
+        for i in range(3):
+            tmp[i], cube[side_idx][edge_idx[i]] = cube[side_idx][edge_idx[i]], tmp[i]
+
+
+for _ in range(int(stdin.readline())):
+    n = int(stdin.readline())
+    cube = [
+        ['w', 'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w'],
+        ['y', 'y', 'y', 'y', 'y', 'y', 'y', 'y', 'y'],
+        ['r', 'r', 'r', 'r', 'r', 'r', 'r', 'r', 'r'],
+        ['o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o'],
+        ['g', 'g', 'g', 'g', 'g', 'g', 'g', 'g', 'g'],
+        ['b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b']
+    ]
+    for oper in stdin.readline().split():
+        rotate(cube, *oper)
+    print("\n".join("".join(islice(cube[0], i*3, i*3 +3)) for i in range(3)))
