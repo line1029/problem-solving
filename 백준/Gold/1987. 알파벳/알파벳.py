90f6e88a0d556b1491ede77ac51845f9cb80764a
line1029 <95160680+line1029@users.noreply.github.com>
2023-06-06 13:42:43 +0900

    [Gold IV] Title: 알파벳, Time: 168 ms, Memory: 115136 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.py
+++ 백준/Gold/1987. 알파벳/알파벳.py
@@ -2,21 +2,22 @@ from sys import stdin
 D = ((1, 0), (0, 1), (-1, 0), (0, -1))
 r, c = map(int, stdin.readline().split())
 grid = stdin.read().splitlines()
-letters = 1 << (ord(grid[0][0]) - 65)
+visited = [[0]*c for _ in range(r)]
+_max = (1 << 26) - 1
 ans = 1
-def dfs(depth, i, j):
-    if depth == 26:
-        print(26)
-        exit()
-    global ans, letters
-    ans = max(ans, depth)
+stack = [(1, 0, 0, 1 << (ord(grid[0][0]) - 65))]
+while stack:
+    depth, i, j, path = stack.pop()
+    if depth > ans:
+        ans = depth
+        if path == _max:
+            print(ans)
+            exit()
     for di, dj in D:
         ni, nj = i + di, j + dj
         if 0<=ni<r and 0<=nj<c:
-            idx = ord(grid[ni][nj]) - 65
-            if not (letters & (1 << idx)):
-                letters |= (1 << idx)
-                dfs(depth + 1, ni, nj)
-                letters &= ~(1 << idx)
-dfs(1, 0, 0)
+            letter = 1 << (ord(grid[ni][nj]) - 65)
+            if not (path & letter) and visited[ni][nj] != (path|letter):
+                visited[ni][nj] = path|letter
+                stack.append((depth + 1, ni, nj, path|letter))
 print(ans)
