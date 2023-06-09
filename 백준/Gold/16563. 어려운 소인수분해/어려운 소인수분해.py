5009744953c08aecaad517f121b5c721dec52b82
line1029 <95160680+line1029@users.noreply.github.com>
2023-06-09 12:42:16 +0900

    [Gold IV] Title: 어려운 소인수분해, Time: 972 ms, Memory: 257292 KB -BaekjoonHub

--- 백준/Gold/16563. 어려운 소인수분해/어려운 소인수분해.py
+++ 백준/Gold/16563. 어려운 소인수분해/어려운 소인수분해.py
@@ -1,21 +1,17 @@
 import os, io, __pypy__
+from math import sqrt
+import struct
 input = io.BytesIO(os.read(0, os.fstat(0).st_size)).readline
 n = int(input())
 nums = list(map(int, input().split()))
 m = max(nums)
-linear_sieve = [0]*(m + 1)
-primes = [2]
-linear_sieve[2::2] = [2]*len(linear_sieve[2::2])
-for i in range(3, m + 1, 2):
-    if not linear_sieve[i]:
-        primes.append(i)
-        linear_sieve[i] = i
-    for j in primes:
-        if j * i > m:
-            break
-        linear_sieve[j * i] = j
-        if i % j == 0:
-            break
+linear_sieve = list(range(m + 1))
+linear_sieve[4::2] = [2]*len(linear_sieve[4::2])
+for i in range(3, int(sqrt(m)) + 1, 2):
+    if linear_sieve[i] == i:
+        for j in range(i*i, m + 1, 2*i):
+            if linear_sieve[j] == j:
+                linear_sieve[j] = i
 ans = __pypy__.builders.StringBuilder()
 for num in nums:
     while num != 1:
