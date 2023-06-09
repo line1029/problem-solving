0b3c986fd18ca57dec8b5fdf7f990bf7adabf4e7
line1029 <95160680+line1029@users.noreply.github.com>
2023-06-09 12:14:17 +0900

    [Gold IV] Title: 어려운 소인수분해, Time: 1072 ms, Memory: 248792 KB -BaekjoonHub

--- 백준/Gold/16563. 어려운 소인수분해/어려운 소인수분해.py
+++ 백준/Gold/16563. 어려운 소인수분해/어려운 소인수분해.py
@@ -1,22 +1,25 @@
-from sys import stdin, stdout
-linear_sieve = [0]*5000001
-primes = []
-for i in range(2, 5000001):
+import os, io, __pypy__
+input = io.BytesIO(os.read(0, os.fstat(0).st_size)).readline
+n = int(input())
+nums = list(map(int, input().split()))
+m = max(nums)
+linear_sieve = [0]*(m + 1)
+primes = [2]
+linear_sieve[2::2] = [2]*len(linear_sieve[2::2])
+for i in range(3, m + 1, 2):
     if not linear_sieve[i]:
         primes.append(i)
         linear_sieve[i] = i
     for j in primes:
-        if j * i > 5000000:
+        if j * i > m:
             break
         linear_sieve[j * i] = j
         if i % j == 0:
             break
-n = int(stdin.readline())
-ans = []
-for num in map(int, stdin.readline().split()):
-    tmp = []
+ans = __pypy__.builders.StringBuilder()
+for num in nums:
     while num != 1:
-        tmp.append(linear_sieve[num])
+        ans.append(f"{linear_sieve[num]} ")
         num //= linear_sieve[num]
-    ans.append(" ".join(map(str, tmp)))
-stdout.write("\n".join(ans))
+    ans.append("\n")
+os.write(1, ans.build().encode())
