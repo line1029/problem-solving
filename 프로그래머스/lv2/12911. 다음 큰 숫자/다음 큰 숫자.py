703112158aa37eb8a164e3eb24916ee4588b241f
line1029 <95160680+line1029@users.noreply.github.com>
2023-06-11 09:37:48 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv2/12911. 다음 큰 숫자/다음 큰 숫자.py
+++ 프로그래머스/lv2/12911. 다음 큰 숫자/다음 큰 숫자.py
@@ -1,17 +1,17 @@
 def solution(n):
-    binary = bin(n)[2:]
+    binary = f"{n:b}"
     ones = binary.count("1")
-    r = len(binary) - 1
+    pointer = len(binary) - 1
     last_zeros = 0
-    while r >= 0 and binary[r] == "0":
+    while pointer >= 0 and binary[pointer] == "0":
         last_zeros += 1
-        r -= 1
+        pointer -= 1
     if last_zeros + ones == len(binary):
         return int("1"+"0"*(last_zeros+1)+"1"*(ones-1), 2)
     last_ones = 0
-    while r >= 0 and binary[r] == "1":
+    while pointer >= 0 and binary[pointer] == "1":
         last_ones += 1
-        r -= 1
-    return int(binary[:r]+"1"+"0"*(last_zeros+1)+"1"*(last_ones-1), 2)
+        pointer -= 1
+    return int(binary[:pointer]+"1"+"0"*(last_zeros+1)+"1"*(last_ones-1), 2)
     
     
