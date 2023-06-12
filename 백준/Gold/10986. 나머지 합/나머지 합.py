95f03d934f3e3f9d6c15a76eb815ada257b4a3a7
line1029 <95160680+line1029@users.noreply.github.com>
2023-06-12 15:32:05 +0900

    [Gold III] Title: 나머지 합, Time: 436 ms, Memory: 217660 KB -BaekjoonHub

--- 백준/Gold/10986. 나머지 합/나머지 합.py
+++ 백준/Gold/10986. 나머지 합/나머지 합.py
@@ -1,5 +1,4 @@
 from sys import stdin
-from math import comb
 n, m = map(int, stdin.readline().split())
 nums = [0] * (n + 1)
 intervals = [0] * m
@@ -9,5 +8,5 @@ for i, num in enumerate(map(int, stdin.readline().split()), 1):
 intervals[0] += 1
 ans = 0
 for i in intervals:
-    ans += comb(i, 2)
+    ans += i*(i-1)//2
 print(ans)
