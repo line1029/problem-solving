68921fc1deada52c1bca1758f76c9b6f3c187363
line1029 <95160680+line1029@users.noreply.github.com>
2023-06-12 21:34:32 +0900

    [Gold III] Title: 역사, Time: 256 ms, Memory: 125020 KB -BaekjoonHub

--- 백준/Gold/1613. 역사/역사.py
+++ 백준/Gold/1613. 역사/역사.py
@@ -1,18 +1,29 @@
-from sys import stdin
+from sys import stdin, stdout
+from collections import deque
 n, k = map(int, stdin.readline().split())
-grid = [[0]*n for _ in range(n)]
+ind = [0]*(n + 1)
+graph = [[] for _ in range(n + 1)]
+history = [0]*(n + 1)
 for _ in range(k):
-    a, b = map(lambda x: int(x) - 1, stdin.readline().split())
-    grid[a][b] = 1
-    grid[b][a] = -1
-s = int(stdin.readline())
-for k in range(n):
-    for i in range(n):
-        for j in range(n):
-            if grid[i][k] == grid[k][j] == 1:
-                grid[i][j] = 1
-                grid[j][i] = -1
-ans = [0]*s
-for i, (a, b) in enumerate(map(lambda x: map(lambda y: int(y) - 1, x.split()), stdin.read().splitlines())):
-    ans[i] = grid[b][a]
-print("\n".join(map(str, ans)))
+    a, b = map(int, stdin.readline().split())
+    graph[a].append(b)
+    ind[b] += 1
+q = deque(i for i, v in enumerate(ind) if i and not v)
+while q:
+    cur = q.popleft()
+    for nex in graph[cur]:
+        history[nex] |= 1 << cur
+        history[nex] |= history[cur]
+        ind[nex] -= 1
+        if not ind[nex]:
+            q.append(nex)
+stdin.readline()
+ans = []
+for a, b in map(lambda x: map(int, x.split()), stdin.read().splitlines()):
+    if history[a] & (1 << b):
+        ans.append(1)
+    elif history[b] & (1 << a):
+        ans.append(-1)
+    else:
+        ans.append(0)
+stdout.write("\n".join(map(str, ans)))
