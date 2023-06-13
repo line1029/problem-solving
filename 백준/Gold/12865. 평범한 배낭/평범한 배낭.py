1050aadf367dc22223d23c02088dd62c264e4a1b
line1029 <95160680+line1029@users.noreply.github.com>
2023-06-13 18:48:55 +0900

    [Gold V] Title: 평범한 배낭, Time: 152 ms, Memory: 115112 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -1,13 +1,12 @@
 from sys import stdin
 n, k = map(int, stdin.readline().split())
 dp = [0]*(k + 1)
-dp[0] = 1
 cur_max = 0
 cur_val = 0
 for w, v in map(lambda x: map(int, x.split()), stdin.read().splitlines()):
     cur_max = min(cur_max + w, k)
     for i in range(cur_max, w - 1, -1):
-        if dp[i - w]:
+        if dp[i - w] or not i - w:
             dp[i] = max(dp[i], dp[i - w] + v)
             cur_val = max(cur_val, dp[i])
-print(max(cur_val - 1, 0))
+print(cur_val)
