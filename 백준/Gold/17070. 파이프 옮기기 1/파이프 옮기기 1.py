3fa172d4acd03b1ed966e01d25c2c9080af23a0d
line1029 <95160680+line1029@users.noreply.github.com>
2023-06-16 00:20:41 +0900

    [Gold V] Title: 파이프 옮기기 1, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.py
+++ 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.py
@@ -1,17 +1,20 @@
 from sys import stdin
 n = int(stdin.readline())
 grid = list(map(lambda x: list(map(int, x.split())), stdin.read().splitlines()))
-D = {0:((0, 1, 0), (1, 1, 2)), 1:((1, 0, 1), (1, 1, 2)), 2:((1, 0, 1), (1, 1, 2), (0, 1, 0))}
-ans = 0
-stack = [(0, 1, 0)]
-while stack:
-    i, j, d = stack.pop()
-    if i == j == n - 1:
-        ans += 1
-    for di, dj, d_nex in D[d]:
-        ni, nj = i + di, j + dj
-        if 0<=ni<n and 0<=nj<n and not grid[ni][nj]:
-            if d_nex == 2 and (grid[ni - 1][nj] or grid[ni][nj - 1]):
+dp0 = [[0]*n for _ in range(n)]
+dp1 = [[0]*n for _ in range(n)]
+dp2 = [[0]*n for _ in range(n)]
+for i in range(1, n):
+    if not grid[0][i]:
+        dp0[0][i] = 1
+    else:
+        break
+for i in range(1, n):
+    for j in range(2, n):
+        if not grid[i][j]:
+            dp0[i][j] += dp0[i][j - 1] + dp2[i][j - 1]
+            dp1[i][j] += dp1[i - 1][j] + dp2[i - 1][j]
+            if grid[i - 1][j] or grid[i][j - 1]:
                 continue
-            stack.append((ni, nj, d_nex))
-print(ans)
+            dp2[i][j] += dp0[i - 1][j - 1] + dp1[i - 1][j - 1] + dp2[i - 1][j - 1]
+print(dp0[-1][-1] + dp1[-1][-1] + dp2[-1][-1])
