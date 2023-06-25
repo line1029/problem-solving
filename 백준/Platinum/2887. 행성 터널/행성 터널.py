78c82d7607db89e450c63f2440d259e782b397f1
Minsoo Jun <95160680+line1029@users.noreply.github.com>
2023-06-25 23:41:40 +0900

    [Platinum V] Title: 행성 터널, Time: 1056 ms, Memory: 94848 KB -BaekjoonHub

--- 백준/Platinum/2887. 행성 터널/행성 터널.py
+++ 백준/Platinum/2887. 행성 터널/행성 터널.py
@@ -1,41 +1,36 @@
 from sys import stdin
-import heapq
-n = int(stdin.readline())
-def get_dist(a, b):
-    a, b = planets[a], planets[b]
-    return min(abs(i - j) for i, j in zip(a, b))
-planets = sorted(map(lambda x: list(map(int, x.split())), stdin.read().splitlines()))
-idx_y = list(range(n))
-idx_y.sort(key=lambda x: planets[x][1])
-inv_y = [0]*n
-for i, num in enumerate(idx_y):
-    inv_y[num] = i
-idx_z = list(range(n))
-idx_z.sort(key=lambda x: planets[x][2])
-inv_z = [0]*n
-for i, num in enumerate(idx_z):
-    inv_z[num] = i
-visited = [0]*n
-pq = [(0, 0)]
-ans = 0
-while pq:
-    dist, cur = heapq.heappop(pq)
-    if visited[cur]:
-        continue
-    visited[cur] = 1
-    ans += dist
-    if cur > 0 and not visited[cur - 1]:
-        heapq.heappush(pq, (get_dist(cur, cur - 1), cur - 1))
-    if cur < n - 1 and not visited[cur + 1]:
-        heapq.heappush(pq, (get_dist(cur, cur + 1), cur + 1))
-    cur_y = inv_y[cur]
-    if cur_y > 0 and not visited[idx_y[cur_y - 1]]:
-        heapq.heappush(pq, (get_dist(cur, idx_y[cur_y - 1]), idx_y[cur_y - 1]))
-    if cur_y < n - 1 and not visited[idx_y[cur_y + 1]]:
-        heapq.heappush(pq, (get_dist(cur, idx_y[cur_y + 1]), idx_y[cur_y + 1]))
-    cur_z = inv_z[cur]
-    if cur_z > 0 and not visited[idx_z[cur_z - 1]]:
-        heapq.heappush(pq, (get_dist(cur, idx_z[cur_z - 1]), idx_z[cur_z - 1]))
-    if cur_z < n - 1 and not visited[idx_z[cur_z + 1]]:
-        heapq.heappush(pq, (get_dist(cur, idx_z[cur_z + 1]), idx_z[cur_z + 1]))
-print(ans)
+def find(p, x):
+    if x != p[x]:
+        p[x] = find(p, p[x])
+    return p[x]
+def union(s, p, x, y):
+    x, y = find(p, x), find(p, y)
+    if x == y:
+        return False
+    if s[x] < s[y]:
+        x, y = y, x
+    s[x] += s[y]
+    p[y] = x
+    return s[x]
+def main():
+    # kruskal
+    n = int(stdin.readline())
+    planets = sorted(map(lambda x: list(map(int, x.split())), stdin.read().splitlines()))
+    idxs = list(range(n))
+    edges = [0]*(n - 1)*3
+    for i in range(3):
+        idxs.sort(key=lambda x: planets[x][i])
+        for j in range(n - 1):
+            edges[i*(n - 1) + j] = (abs(planets[idxs[j]][i] - planets[idxs[j + 1]][i]), idxs[j], idxs[j + 1])
+    edges.sort()
+    parent = list(range(n))
+    size = [1]*n
+    ans = 0
+    for e, i, j in edges:
+        cnt = union(size, parent, i, j)
+        if cnt:
+            ans += e
+        if cnt == n:
+            break
+    return ans
+print(main())
