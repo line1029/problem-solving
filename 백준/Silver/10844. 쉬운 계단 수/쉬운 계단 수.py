cc12611d8bdcc919f2cc0ba998f2a6a92f9331a8
Minsoo Jun <95160680+line1029@users.noreply.github.com>
2023-07-01 23:51:59 +0900

    [Silver I] Title: 쉬운 계단 수, Time: 40 ms, Memory: 31388 KB -BaekjoonHub

---
+++ 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.py
@@ -0,0 +1,9 @@
+n = int(input())
+dp = [[0]*10 for _ in range(n)]
+dp[0] = [1]*10
+for i in range(1, n):
+    dp[i][0] += dp[i - 1][1]
+    dp[i][9] += dp[i - 1][8]
+    for j in range(1, 9):
+        dp[i][j] += dp[i - 1][j - 1] + dp[i - 1][j + 1]
+print(sum(dp[-1][1:])%1_000_000_000)
