7d6242b37c72b76d78fd64740f773e5a569f80fc
Minsoo Jun <95160680+line1029@users.noreply.github.com>
2023-07-03 20:30:32 +0900

    [Gold III] Title: 반전 수와 쿼리, Time: 152 ms, Memory: 42496 KB -BaekjoonHub

--- 백준/Gold/25339. 반전 수와 쿼리/반전 수와 쿼리.py
+++ 백준/Gold/25339. 반전 수와 쿼리/반전 수와 쿼리.py
@@ -1,9 +1,11 @@
 from sys import stdin, stdout
-n, q = map(int, stdin.readline().split())
-ans = []
-inv = 0
-for a, l, r in map(lambda x: map(int, x.split()), stdin.read().splitlines()):
-    if a == 1 or ((r - l + 1) >> 1) & 1:
-        inv = 0 if inv else 1
-    ans.append(inv)
-stdout.write("\n".join(map(str, ans)))
+def main():
+    n, q = map(int, stdin.readline().split())
+    ans = []
+    inv = 0
+    for a, l, r in map(lambda x: map(int, x.split()), stdin.read().splitlines()):
+        if a == 1 or ((r - l + 1) >> 1) & 1:
+            inv = 0 if inv else 1
+        ans.append(inv)
+    stdout.write("\n".join(map(str, ans)))
+main()
