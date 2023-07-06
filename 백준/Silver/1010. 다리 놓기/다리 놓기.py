ebc08ac2c543aa0754e107d26e39cd5bbcaab532
Minsoo Jun <95160680+line1029@users.noreply.github.com>
2023-07-06 12:45:17 +0900

    [Silver V] Title: 다리 놓기, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1010. 다리 놓기/다리 놓기.py
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.py
@@ -1,10 +1,15 @@
 from sys import stdin, stdout
-from math import comb
+
 def main():
     stdin.readline()
+    dp = [[0]*31 for _ in range(31)]
+    for i in range(31):
+        dp[i][0] = dp[i][i] = 1
+        for j in range(1, i):
+            dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j]
     ans = []
     for n, m in map(lambda x: map(int, x.split()), stdin.read().splitlines()):
-        ans.append(comb(m, n))
+        ans.append(dp[m][n])
     stdout.write("\n".join(map(str, ans)))
 
 main()
