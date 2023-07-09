6e1fd4bec00a978e35ece47b8d2d5d0c331f8a5f
Minsoo Jun <95160680+line1029@users.noreply.github.com>
2023-07-09 14:06:02 +0900

    [Platinum V] Title: 다이아몬드 광산, Time: 432 ms, Memory: 125884 KB -BaekjoonHub

--- 백준/Platinum/1028. 다이아몬드 광산/다이아몬드 광산.py
+++ 백준/Platinum/1028. 다이아몬드 광산/다이아몬드 광산.py
@@ -1,29 +1,33 @@
 from sys import stdin
 r, c = map(int, stdin.readline().split())
-grid = list(map(lambda x: list(map(int, x)), stdin.read().splitlines()))
-if not max(max(i) for i in grid):
-    print(0)
-    exit()
-down_right = [i[:] for i in grid]
-down_left = [i[:] for i in grid]
-for i in range(1, r):
-    for j in range(c):
-        if grid[i][j]:
-            if j:
-                down_right[i][j] = down_right[i - 1][j - 1] + 1
-            if j < c - 1:
-                down_left[i][j] = down_left[i - 1][j + 1] + 1
-max_size = (min(r, c) + 1) >> 1
-ans = 1
+grid = [stdin.readline().rstrip() for _ in range(r)]
+down_right = [[0]*c for _ in range(r)]
+down_left = [[0]*c for _ in range(r)]
 for i in range(r):
     for j in range(c):
+        if grid[i][j] == "0":
+            continue
+        if i and j:
+            down_right[i][j] = down_right[i - 1][j - 1] + 1
+        else:
+            down_right[i][j] = 1
+        if i and j < c - 1:
+            down_left[i][j] = down_left[i - 1][j + 1] + 1
+        else:
+            down_left[i][j] = 1
+max_size = (min(r, c) + 1) >> 1
+ans = 0
+for i in range(r):
+    for j in range(c):
+        if grid[i][j] == "0":
+            continue
         _size = min(down_left[i][j], down_right[i][j])
         if _size <= ans:
             continue
-        for k in range(_size - 1, 0, -1):
-            if min(down_left[i - k][j - k], down_right[i - k][j + k]) >= k + 1:
-                ans = max(ans, k + 1)
+        for k in range(_size, 0, -1):
+            if min(down_left[i - k + 1][j - k + 1], down_right[i - k + 1][j + k - 1]) >= k:
+                ans = max(ans, k)
                 break
-            if k < ans:
+            if k <= ans:
                 break
 print(ans)
