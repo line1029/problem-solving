466362a9c4aa445d0b849ab4246fba7c37644503
Minsoo Jun <95160680+line1029@users.noreply.github.com>
2023-07-10 12:56:41 +0900

    [Gold II] Title: 플로이드 2, Time: 208 ms, Memory: 118332 KB -BaekjoonHub

--- 백준/Gold/11780. 플로이드 2/플로이드 2.py
+++ 백준/Gold/11780. 플로이드 2/플로이드 2.py
@@ -10,35 +10,18 @@ for i in range(1, n + 1):
 for _ in range(m):
     a, b, cost = map(int, input().split())
     adj_mat[a][b] = min(adj_mat[a][b], cost)
-    transit_mat[a][b] = 0
+    transit_mat[a][b] = b
 for k in range(1, n + 1):
     for i in range(1, n + 1):
         for j in range(1, n + 1):
             if adj_mat[i][k] != INF and adj_mat[k][j] != INF and adj_mat[i][k] + adj_mat[k][j] < adj_mat[i][j]:
                 adj_mat[i][j] = adj_mat[i][k] + adj_mat[k][j]
-                transit_mat[i][j] = k
+                transit_mat[i][j] = transit_mat[i][k]
 for i in range(1, n + 1):
     for j in range(1, n + 1):
         if adj_mat[i][j] == INF:
             adj_mat[i][j] = 0
-def dfs(i, j):
-    if path_mat[i][j]:
-        return path_mat[i][j]
-    if not transit_mat[i][j]:
-        path_mat[i][j] = [i]
-        return path_mat[i][j]
-    if transit_mat[i][j] == INF:
-        return
-    path = []
-    path.extend(dfs(i, transit_mat[i][j]))
-    path.extend(dfs(transit_mat[i][j], j))
-    path_mat[i][j] = path
-    return path
 
-for i in range(1, n + 1):
-    for j in range(1, n + 1):
-        if adj_mat[i][j] < INF:
-            dfs(i, j)
 for i in range(1, n + 1):
     for j in range(1, n + 1):
         if path_mat[i][j]:
@@ -49,9 +32,15 @@ for i in range(1, n + 1):
     ans.append("\n")
 for i in range(1, n + 1):
     for j in range(1, n + 1):
-        if path_mat[i][j]:
-            ans.append(f"{len(path_mat[i][j])} ")
-            ans.append(" ".join(map(str, path_mat[i][j])))
+        if transit_mat[i][j] != INF:
+            path = [i]
+            ptr = transit_mat[i][j]
+            while ptr != j:
+                path.append(ptr)
+                ptr = transit_mat[ptr][j]
+            path.append(j)
+            ans.append(f"{len(path)} ")
+            ans.append(" ".join(map(str, path)))
         else:
             ans.append("0")
         ans.append("\n")
