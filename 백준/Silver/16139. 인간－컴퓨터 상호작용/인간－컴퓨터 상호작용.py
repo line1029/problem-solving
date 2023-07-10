301579eb5276c56144d758d95b158ae886cf66bf
Minsoo Jun <95160680+line1029@users.noreply.github.com>
2023-07-10 15:14:29 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 964 ms, Memory: 257492 KB -BaekjoonHub

--- 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.py
+++ 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.py
@@ -1,17 +1,20 @@
-import os, io, __pypy__
+import os, io
+from sys import stdout
 input = io.BytesIO(os.read(0, os.fstat(0).st_size)).readline
-s = input().rstrip()
-n = len(s)
-dp = [[0]*(n + 1) for _ in range(26)]
-for j, i in enumerate(map(lambda x: x - 97, s)):
-    dp[i][j] += 1
-for i in range(26):
-    for j in range(n - 1):
-        dp[i][j + 1] += dp[i][j]
+def main():
+    s = input().rstrip()
+    n = len(s)
+    dp = [[0]*(n + 1) for _ in range(26)]
+    for j, i in enumerate(map(lambda x: x - 97, s)):
+        dp[i][j] += 1
+    for i in range(26):
+        for j in range(n - 1):
+            dp[i][j + 1] += dp[i][j]
 
-q = int(input())
-ans = __pypy__.builders.StringBuilder()
-for a, i, j in (input().split() for _ in range(q)):
-    a, i, j = int.from_bytes(a, "little") - 97, int(i), int(j)
-    ans.append(f"{dp[a][j] - dp[a][i - 1]}\n")
-os.write(1, ans.build().encode())
+    q = int(input())
+    ans = []
+    for a, i, j in (input().split() for _ in range(q)):
+        a, i, j = int.from_bytes(a, "little") - 97, int(i), int(j)
+        ans.append(dp[a][j] - dp[a][i - 1])
+    stdout.write("\n".join(map(str, ans)))
+main()
