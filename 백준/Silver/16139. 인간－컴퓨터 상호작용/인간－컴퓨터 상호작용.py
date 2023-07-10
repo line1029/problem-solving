cc2a95365f51826cc2d3cd7ea518a1b164cebec4
Minsoo Jun <95160680+line1029@users.noreply.github.com>
2023-07-10 15:17:25 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 52 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.py
+++ 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.py
@@ -4,17 +4,17 @@ input = io.BytesIO(os.read(0, os.fstat(0).st_size)).readline
 def main():
     s = input().rstrip()
     n = len(s)
-    dp = [[0]*(n + 1) for _ in range(26)]
+    dp = [[0]*26 for _ in range(n + 1)]
     for j, i in enumerate(map(lambda x: x - 97, s)):
-        dp[i][j] += 1
+        dp[j][i] += 1
     for i in range(26):
         for j in range(n - 1):
-            dp[i][j + 1] += dp[i][j]
+            dp[j + 1][i] += dp[j][i]
 
     q = int(input())
     ans = []
     for a, i, j in (input().split() for _ in range(q)):
         a, i, j = int.from_bytes(a, "little") - 97, int(i), int(j)
-        ans.append(dp[a][j] - dp[a][i - 1])
+        ans.append(dp[j][a] - dp[i - 1][a])
     stdout.write("\n".join(map(str, ans)))
 main()
