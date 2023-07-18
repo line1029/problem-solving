2eac0491c3c14c047442a32772b6c19e16ac3ffc
Minsoo Jun <95160680+line1029@users.noreply.github.com>
2023-07-18 22:23:16 +0900

    [Gold V] Title: 2의 멱수의 합, Time: 124 ms, Memory: 118236 KB -BaekjoonHub

--- 백준/Gold/2410. 2의 멱수의 합/2의 멱수의 합.py
+++ 백준/Gold/2410. 2의 멱수의 합/2의 멱수의 합.py
@@ -3,7 +3,8 @@ if n <= 2:
     print(n)
     exit()
 m = 1_000_000_000
-dp = [1]*(n + 2)
-for i in range(2, n + 1, 2):
-    dp[i] = dp[i + 1] = (dp[i - 1] + dp[i >> 1])%m
+n >>= 1
+dp = [1]*(n + 1)
+for i in range(1, n + 1):
+    dp[i] = (dp[i - 1] + dp[i >> 1])%m
 print(dp[n])
