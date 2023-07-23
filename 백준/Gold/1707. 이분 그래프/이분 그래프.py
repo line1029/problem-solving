239ac22d3c4cbcf0fa21e942fc652d461125216a
Minsoo Jun <95160680+line1029@users.noreply.github.com>
2023-07-23 21:52:24 +0900

    [Gold IV] Title: 이분 그래프, Time: 604 ms, Memory: 196964 KB -BaekjoonHub

--- 백준/Gold/1707. 이분 그래프/이분 그래프.py
+++ 백준/Gold/1707. 이분 그래프/이분 그래프.py
@@ -1,11 +1,12 @@
-from sys import stdin
+import io, os
 from collections import deque
+input = io.BytesIO(os.read(0, os.fstat(0).st_size)).readline
 def bfs():
-    v, e = map(int, stdin.readline().split())
+    v, e = map(int, input().split())
     graph = [[] for _ in range(v + 1)]
     visited = [0]*(v + 1)
     for _ in range(e):
-        a, b = map(int, stdin.readline().split())
+        a, b = map(int, input().split())
         graph[a].append(b)
         graph[b].append(a)
     for i in range(1, v + 1):
@@ -24,7 +25,7 @@ def bfs():
                         elif visited[nex] != (j&1) + 1:
                             return True
 
-k = int(stdin.readline())
+k = int(input())
 ans = ["YES"]*k
 for i in range(k):
     if bfs():
