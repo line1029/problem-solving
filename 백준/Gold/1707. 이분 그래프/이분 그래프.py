314a821b3ae50de3efcff1425b40a841530e1b58
Minsoo Jun <95160680+line1029@users.noreply.github.com>
2023-07-23 21:56:27 +0900

    [Gold IV] Title: 이분 그래프, Time: 500 ms, Memory: 192548 KB -BaekjoonHub

--- 백준/Gold/1707. 이분 그래프/이분 그래프.py
+++ 백준/Gold/1707. 이분 그래프/이분 그래프.py
@@ -1,29 +1,25 @@
 import io, os
-from collections import deque
 input = io.BytesIO(os.read(0, os.fstat(0).st_size)).readline
 def bfs():
     v, e = map(int, input().split())
     graph = [[] for _ in range(v + 1)]
-    visited = [0]*(v + 1)
+    visited = [-1]*(v + 1)
     for _ in range(e):
         a, b = map(int, input().split())
         graph[a].append(b)
         graph[b].append(a)
     for i in range(1, v + 1):
-        if not visited[i]:
-            q = deque([i])
-            visited[i] = 1
-            for j in range(1, v*e + 2):
-                if not q:
-                    break
-                for _ in range(len(q)):
-                    cur = q.popleft()
-                    for nex in graph[cur]:
-                        if not visited[nex]:
-                            visited[nex] = (j&1) + 1
-                            q.append(nex)
-                        elif visited[nex] != (j&1) + 1:
-                            return True
+        if visited[i] == -1:
+            st = [i]
+            visited[i] = 0
+            while st:
+                cur = st.pop()
+                for nex in graph[cur]:
+                    if visited[nex] == -1:
+                        visited[nex] = visited[cur]^1
+                        st.append(nex)
+                    elif visited[nex] == visited[cur]:
+                        return True
 
 k = int(input())
 ans = ["YES"]*k
