8b008b5ca8a1f40d99b926a3ca96fe3744e3892d
Minsoo Jun <95160680+line1029@users.noreply.github.com>
2023-07-26 13:23:27 +0900

    [Gold IV] Title: Count Circle Groups, Time: 2276 ms, Memory: 124616 KB -BaekjoonHub

--- 백준/Gold/10216. Count Circle Groups/Count Circle Groups.py
+++ 백준/Gold/10216. Count Circle Groups/Count Circle Groups.py
@@ -1,29 +1,24 @@
-from sys import stdin
+import os, io
+input = io.BytesIO(os.read(0,os.fstat(0).st_size)).readline
 def find(x):
-    if parent[x] != x:
-        parent[x] = find(parent[x])
-    return parent[x]
-
-def union(x, y):
-    x, y = find(x), find(y)
-    if x == y:
-        return
-    if size[x] < size[y]:
-        x, y = y, x
-    parent[y] = x
-    size[x] += size[y]
+    while parent[x] != x:
+        x, parent[x] = parent[x], parent[parent[x]]
+    return x
 
 ans = []
-for _ in range(int(stdin.readline())):
-    n = int(stdin.readline())
+for _ in range(int(input())):
+    n = int(input())
     parent = list(range(n))
-    size = [1]*n
     x, y, r = [0]*n, [0]*n, [0]*n
     for i in range(n):
-        x[i], y[i], r[i] = map(int, stdin.readline().split())
+        x[i], y[i], r[i] = map(int, input().split())
     for i in range(n - 1):
         for j in range(i + 1, n):
             if (x[i] - x[j])**2 + (y[i] - y[j])**2 <= (r[i] + r[j])**2:
-                union(i, j)
+                a, b = find(i), find(j)
+                if a > b:
+                    parent[b] = a
+                else:
+                    parent[a] = b
     ans.append(sum(parent[i] == i for i in range(n)))
 print("\n".join(map(str, ans)))
