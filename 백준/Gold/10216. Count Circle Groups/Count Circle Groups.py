7805d778bdbbb12148031549c0d5c97efb35aebd
Minsoo Jun <95160680+line1029@users.noreply.github.com>
2023-07-26 12:51:52 +0900

    [Gold IV] Title: Count Circle Groups, Time: 2332 ms, Memory: 116824 KB -BaekjoonHub

--- 백준/Gold/10216. Count Circle Groups/Count Circle Groups.py
+++ 백준/Gold/10216. Count Circle Groups/Count Circle Groups.py
@@ -4,26 +4,18 @@ def find(x):
         parent[x] = find(parent[x])
     return parent[x]
 
-def union(x, y):
-    x, y = find(x), find(y)
-    if x == y:
-        return
-    if size[x] < size[y]:
-        x, y = y, x
-    parent[y] = x
-    size[x] += size[y]
-
 ans = []
 for _ in range(int(stdin.readline())):
     n = int(stdin.readline())
     parent = list(range(n))
-    size = [1]*n
-    enemies = [list(map(int, stdin.readline().split())) for _ in range(n)]
+    x, y, r = [0]*n, [0]*n, [0]*n
+    for i in range(n):
+        x[i], y[i], r[i] = map(int, stdin.readline().split())
     for i in range(n - 1):
-        x1, y1, r1 = enemies[i]
         for j in range(i + 1, n):
-            x2, y2, r2 = enemies[j]
-            if (x1 - x2)*(x1 - x2) + (y1 - y2)*(y1 - y2) <= (r1 + r2)*(r1 + r2):
-                union(i, j)
-    ans.append(sum(parent[x] == x for x in range(n)))
+            if (x[i] - x[j])**2 + (y[i] - y[j])**2 <= (r[i] + r[j])**2:
+                a, b = find(i), find(j)
+                if a != b:
+                    parent[b] = a
+    ans.append(sum(parent[i] == i for i in range(n)))
 print("\n".join(map(str, ans)))
