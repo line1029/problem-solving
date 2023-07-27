3dd38f1f964ff2e8c9ffb3d47bfcf91286cd2e39
Minsoo Jun <95160680+line1029@users.noreply.github.com>
2023-07-27 09:54:07 +0900

    [Gold V] Title: 여러분의 다리가 되어 드리겠습니다!, Time: 260 ms, Memory: 126004 KB -BaekjoonHub

--- 백준/Gold/17352. 여러분의 다리가 되어 드리겠습니다！/여러분의 다리가 되어 드리겠습니다！.py
+++ 백준/Gold/17352. 여러분의 다리가 되어 드리겠습니다！/여러분의 다리가 되어 드리겠습니다！.py
@@ -3,9 +3,9 @@ input = io.BytesIO(os.read(0,os.fstat(0).st_size)).readline
 n = int(input())
 parent = list(range(n + 1))
 def find(x):
-    if parent[x] != x:
-        parent[x] = find(parent[x])
-    return parent[x]
+    while parent[x] != x:
+        x, parent[x] = parent[x], parent[parent[x]]
+    return x
 for _ in range(n - 2):
     a, b = map(lambda x: find(int(x)), input().split())
     if a < b:
