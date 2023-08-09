b3c3f2251ab682971e2ccb927d3cccfbc1391cb5
Minsoo Jun <95160680+line1029@users.noreply.github.com>
2023-08-09 14:51:19 +0900

    [Gold III] Title: 벽 부수고 이동하기 2, Time: 1900 ms, Memory: 245796 KB -BaekjoonHub

--- 백준/Gold/14442. 벽 부수고 이동하기 2/벽 부수고 이동하기 2.py
+++ 백준/Gold/14442. 벽 부수고 이동하기 2/벽 부수고 이동하기 2.py
@@ -1,31 +1,30 @@
-from sys import stdin
+import io, os
+input = io.BytesIO(os.read(0, os.fstat(0).st_size)).readline
 from collections import deque
 D = ((0, 1), (0, -1), (1, 0), (-1, 0))
 INF = 1000001
-n, m, k = map(int, stdin.readline().split())
-grid = stdin.read().splitlines()
-visited = [0]*(m*n)
-steps = [INF]*(m*n)
-steps[0] = 0
-q = deque([[0, 0, 0, 0]])
-while q:
-    i, j, step, eli = q.popleft()
-    for di, dj in D:
-        ni, nj = i + di, j + dj
-        if 0 <= ni < n and 0 <= nj < m:
-            if grid[ni][nj] == "1":
-                if eli == k or (visited[ni*m + nj] & ((1 << (eli + 2)) - 1)): continue
-                visited[ni*m + nj] |= (1 << (eli + 1))
-                if steps[ni*m + nj] == INF:
-                    steps[ni*m + nj] = step + 1
-                q.append([ni, nj, step + 1, eli + 1])
-            else:
-                if (visited[ni*m + nj] & ((1 << (eli + 1)) - 1)): continue
-                visited[ni*m + nj] |= (1 << eli)
-                if steps[ni*m + nj] == INF:
-                    steps[ni*m + nj] = step + 1
-                q.append([ni, nj, step + 1, eli])
-if steps[-1] != INF:
-    print(steps[-1] + 1)
-else:
-    print(-1)
+n, m, k = map(int, input().split())
+if n == m == 1:
+    print(1)
+    exit()
+grid = []
+for _ in range(n):
+    grid += list(map(lambda x: x - 48, input().strip()))
+visited = [k + 1]*(m*n)
+q = deque([[0, 0, 0]])
+for step in range(m*n + 1):
+    if not q:
+        break
+    for _ in range(len(q)):
+        i, j, eli = q.popleft()
+        for di, dj in D:
+            ni, nj = i + di, j + dj
+            if 0 <= ni < n and 0 <= nj < m:
+                if eli + grid[ni*m + nj] > k: continue
+                if visited[ni*m + nj] > eli + grid[ni*m + nj]:
+                    if ni == n - 1 and nj == m - 1:
+                        print(step + 2)
+                        exit()
+                    visited[ni*m + nj] = eli + grid[ni*m + nj]
+                    q.append((ni, nj, eli + grid[ni*m + nj]))
+print(-1)
