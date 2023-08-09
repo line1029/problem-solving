91db85bb3f33c0dd2cfbe06aebdc0b884931de5b
Minsoo Jun <95160680+line1029@users.noreply.github.com>
2023-08-09 14:23:47 +0900

    [Gold III] Title: 벽 부수고 이동하기 2, Time: 2372 ms, Memory: 254516 KB -BaekjoonHub

--- 백준/Gold/14442. 벽 부수고 이동하기 2/벽 부수고 이동하기 2.py
+++ 백준/Gold/14442. 벽 부수고 이동하기 2/벽 부수고 이동하기 2.py
@@ -4,8 +4,9 @@ D = ((0, 1), (0, -1), (1, 0), (-1, 0))
 INF = 1000001
 n, m, k = map(int, stdin.readline().split())
 grid = stdin.read().splitlines()
-visited = [[[INF, 0] for _ in range(m)] for _ in range(n)]
-visited[0][0][0] = 0
+visited = [[0]*m for _ in range(n)]
+steps = [[INF]*m for _ in range(n)]
+steps[0][0] = 0
 q = deque([[0, 0, 0, 0]])
 while q:
     i, j, step, eli = q.popleft()
@@ -13,18 +14,18 @@ while q:
         ni, nj = i + di, j + dj
         if 0 <= ni < n and 0 <= nj < m:
             if grid[ni][nj] == "1":
-                if eli == k or (visited[ni][nj][1] & ((1 << (eli + 2)) - 1)): continue
-                visited[ni][nj][1] |= (1 << (eli + 1))
-                if visited[ni][nj][0] == INF:
-                    visited[ni][nj][0] = step + 1
+                if eli == k or (visited[ni][nj] & ((1 << (eli + 2)) - 1)): continue
+                visited[ni][nj] |= (1 << (eli + 1))
+                if steps[ni][nj] == INF:
+                    steps[ni][nj] = step + 1
                 q.append([ni, nj, step + 1, eli + 1])
             else:
-                if (visited[ni][nj][1] & ((1 << (eli + 1)) - 1)): continue
-                visited[ni][nj][1] |= (1 << eli)
-                if visited[ni][nj][0] == INF:
-                    visited[ni][nj][0] = step + 1
+                if (visited[ni][nj] & ((1 << (eli + 1)) - 1)): continue
+                visited[ni][nj] |= (1 << eli)
+                if steps[ni][nj] == INF:
+                    steps[ni][nj] = step + 1
                 q.append([ni, nj, step + 1, eli])
-if visited[-1][-1][0] != INF:
-    print(visited[-1][-1][0] + 1)
+if steps[-1][-1] != INF:
+    print(steps[-1][-1] + 1)
 else:
     print(-1)
