730291d72080e0fcdf93276c549623b4c438846f
Minsoo Jun <95160680+line1029@users.noreply.github.com>
2023-08-14 11:40:47 +0900

    [Bronze II] Title: Affine Cipher, Time: 128 ms, Memory: 33460 KB -BaekjoonHub

--- 백준/Bronze/11575. Affine Cipher/Affine Cipher.py
+++ 백준/Bronze/11575. Affine Cipher/Affine Cipher.py
@@ -1,15 +1,11 @@
 from sys import stdin, stdout
 def main():
-    ans = []
     origin = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     for _ in range(int(stdin.readline())):
         a, b = map(int, stdin.readline().split())
-        a %= 26
-        b %= 26
         t = str.maketrans(
             origin,
             "".join(origin[(a*i + b)%26] for i in range(26))
         )
-        ans.append(stdin.readline().translate(t))
-    stdout.write("".join(ans))
+        stdout.write(stdin.readline().translate(t))
 main()
