315e0b0c9e205c1774af3220e60ef654a579cd42
Minsoo Jun <95160680+line1029@users.noreply.github.com>
2023-08-17 14:09:27 +0900

    [Silver I] Title: 진심 좌우 반복뛰기, Time: 164 ms, Memory: 118764 KB -BaekjoonHub

--- 백준/Silver/23631. 진심 좌우 반복뛰기/진심 좌우 반복뛰기.py
+++ 백준/Silver/23631. 진심 좌우 반복뛰기/진심 좌우 반복뛰기.py
@@ -1,19 +1,17 @@
-import io, os
+import io, os, __pypy__
 from math import sqrt
 input = io.BytesIO(os.read(0, os.fstat(0).st_size)).readline
-def main():
-    t = int(input())
-    ans = [0]*t
-    for i in range(t):
-        # k*x*(x + 1)//2 <= n < k*(x + 1)*(x + 2)//2
-        # kx^2 + kx - 2n <= 0
-        # x = (-k +- sqrt(k^2 + 8kn))/2k
-        n, k = map(int, input().split())
-        n -= 1
-        x = int((-k + sqrt(k*k + 8*k*n))/(2*k))
-        if x&1:
-            ans[i] = f"{k*(x + 1)*(x + 1)//2 - n} L"
-        else:
-            ans[i] = f"{n - k*x*(x + 2)//2} R"
-    print("\n".join(ans))
-main()
+t = int(input())
+ans = __pypy__.builders.StringBuilder()
+for i in range(t):
+    # k*x*(x + 1)//2 <= n < k*(x + 1)*(x + 2)//2
+    # kx^2 + kx - 2n <= 0
+    # x = (-k +- sqrt(k^2 + 8kn))/2k
+    n, k = map(int, input().split())
+    n -= 1
+    x = int((-k + sqrt(k*k + 8*k*n))/(2*k))
+    if x&1:
+        ans.append(f"{k*(x + 1)*(x + 1)//2 - n} L\n")
+    else:
+        ans.append(f"{n - k*x*(x + 2)//2} R\n")
+os.write(1, ans.build().encode())
