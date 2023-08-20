76427b1a17d324d9f95c68f6bc9966f49cd99410
Minsoo Jun <95160680+line1029@users.noreply.github.com>
2023-08-20 21:02:34 +0900

    [Gold I] Title: K번째 수, Time: 212 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Gold/1300. K번째 수/K번째 수.py
+++ 백준/Gold/1300. K번째 수/K번째 수.py
@@ -1,11 +1,13 @@
 from sys import stdin
-n, k = map(int, stdin.read().splitlines())
-lo, hi = 1, k
-while lo < hi:
-    mid = (lo + hi) >> 1
-    sqmid = int(mid**.5)
-    if 2*sum(min(n, mid//i) for i in range(1, sqmid + 1)) - sqmid*sqmid >= k:
-        hi = mid
-    else:
-        lo = mid + 1
-print(lo)
+def main():
+    n, k = map(int, stdin.read().splitlines())
+    lo, hi = 1, k
+    while lo < hi:
+        mid = (lo + hi) >> 1
+        sqmid = int(mid**.5)
+        if 2*sum(min(n, mid//i) for i in range(1, sqmid + 1)) - sqmid*sqmid >= k:
+            hi = mid
+        else:
+            lo = mid + 1
+    print(lo)
+main()
