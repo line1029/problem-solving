55b2a359fb913a36a98384409d14a72bb311699d
Minsoo Jun <95160680+line1029@users.noreply.github.com>
2023-09-03 00:07:22 +0900

    [Silver I] Title: 부동산 다툼, Time: 352 ms, Memory: 63768 KB -BaekjoonHub

--- 백준/Silver/20364. 부동산 다툼/부동산 다툼.py
+++ 백준/Silver/20364. 부동산 다툼/부동산 다툼.py
@@ -1,18 +1,18 @@
-import os, io, __pypy__
+import os, io
 input = io.BytesIO(os.read(0, os.fstat(0).st_size)).readline
-n, q = map(int, input().split())
-tree = [0]*(n + 1)
-ans = __pypy__.builders.StringBuilder()
-for _ in range(q):
-    i = wanted = int(input())
-    x = 0
-    while i:
-        if tree[i]:
-            x = i
-        i >>= 1
-    if x:
-        ans.append(f"{x}\n")
-    else:
-        ans.append("0\n")
-        tree[wanted] = 1
-os.write(1, ans.build().encode())
+def main():
+    n, q = map(int, input().split())
+    tree = [0]*(n + 1)
+    ans = [0]*q
+    for idx in range(q):
+        i = wanted = int(input())
+        x = 0
+        while i:
+            if tree[i]:
+                x = i
+            i >>= 1
+        ans[idx] = x
+        if not x:
+            tree[wanted] = 1
+    print("\n".join(map(str, ans)))
+main()
