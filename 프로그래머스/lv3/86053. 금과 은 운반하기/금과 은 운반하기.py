f304bd7ecd3d0cb718da4be606fee9ec5b7c632b
Minsoo Jun <95160680+line1029@users.noreply.github.com>
2023-09-03 00:38:21 +0900

    [level 3] Title: 금과 은 운반하기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv3/86053. 금과 은 운반하기/금과 은 운반하기.py
+++ 프로그래머스/lv3/86053. 금과 은 운반하기/금과 은 운반하기.py
@@ -6,9 +6,9 @@ def solution(a, b, g, s, w, t):
         weight_total = weight_gold = weight_silver = 0
         for i in range(n):
             weight = ((mid - t[i])//(t[i] << 1) + 1)*w[i]
-            weight_total += weight if weight < g[i] + s[i] else g[i] + s[i]
-            weight_gold += weight if weight < g[i] else g[i]
-            weight_silver += weight if weight < s[i] else s[i]
+            weight_total += min(weight, g[i] + s[i])
+            weight_gold += min(weight, g[i])
+            weight_silver += min(weight, s[i])
         if weight_total >= a + b and weight_gold >= a and weight_silver >= b:
             hi = mid
         else:
