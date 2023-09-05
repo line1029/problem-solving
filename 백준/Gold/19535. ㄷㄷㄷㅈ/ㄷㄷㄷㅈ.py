fa9a2ccf58ca32d96882cb4a063c3b8c55b166fc
Minsoo Jun <95160680+line1029@users.noreply.github.com>
2023-09-06 01:29:33 +0900

    [Gold III] Title: ㄷㄷㄷㅈ, Time: 272 ms, Memory: 148680 KB -BaekjoonHub

--- 백준/Gold/19535. ㄷㄷㄷㅈ/ㄷㄷㄷㅈ.py
+++ 백준/Gold/19535. ㄷㄷㄷㅈ/ㄷㄷㄷㅈ.py
@@ -1,21 +1,20 @@
 import os, io
+from math import comb
 input = io.BytesIO(os.read(0, os.fstat(0).st_size)).readline
 n = int(input())
-graph = [[] for _ in range(n + 1)]
-for _ in range(n - 1):
+graph = [0]*(n + 1)
+edges = [0]*(n - 1)
+for i in range(n - 1):
     a, b = map(int, input().split())
-    graph[a].append(b)
-    graph[b].append(a)
+    graph[a] += 1
+    graph[b] += 1
+    edges[i] = [a, b]
 D_tree = G_tree = 0
-g = dict()
 for i in range(1, n + 1):
-    if (k:=len(graph[i])) == 1: continue
-    G_tree += k*(k - 1)*(k - 2)//6
-    for j in graph[i]:
-        if j < i: continue
-        if (l:= len(graph[j])) != 1:
-            D_tree += (k - 1)*(l - 1)
+    G_tree += comb(graph[i], 3)
 G_tree *= 3
+for a, b in edges:
+    D_tree += (graph[a] - 1)*(graph[b] - 1)
 if D_tree > G_tree:
     print("D")
 elif D_tree < G_tree:
