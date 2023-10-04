8ac49c6469bfe347b2f61c0d5255890377814225
Minsoo Jun <95160680+line1029@users.noreply.github.com>
2023-10-05 05:03:55 +0900

    [Gold V] Title: 흙길 보수하기, Time: 56 ms, Memory: 33300 KB -BaekjoonHub

--- 백준/Gold/1911. 흙길 보수하기/흙길 보수하기.py
+++ 백준/Gold/1911. 흙길 보수하기/흙길 보수하기.py
@@ -1,12 +1,14 @@
 from sys import stdin
-n, l = map(int, stdin.readline().split())
-pool = sorted(map(lambda x: list(map(int, x.split())), stdin.read().splitlines()))
-cur = pool[0][0] + l
-ans = 1
-for start, end in pool:
-    if end <= cur: continue
-    if start < cur: start = cur
-    need = (end - start)//l + ((end - start)%l != 0)
-    ans += need
-    cur = start + need*l
-print(ans)
+def main():
+    n, l = map(int, stdin.readline().split())
+    pool = sorted(map(lambda x: list(map(int, x.split())), stdin.read().splitlines()))
+    cur = pool[0][0] + l
+    ans = 1
+    for start, end in pool:
+        if end <= cur: continue
+        if start < cur: start = cur
+        need = (end - start)//l + ((end - start)%l != 0)
+        ans += need
+        cur = start + need*l
+    print(ans)
+main()
