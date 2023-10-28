35dac21343533736c99dd65bf246eba322d2384e
Minsoo Jun <95160680+line1029@users.noreply.github.com>
2023-10-28 20:24:54 +0900

    [Bronze IV] Title: Winning Score, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/17009. Winning Score/Winning Score.py
@@ -0,0 +1,8 @@
+a = int(input())*3 + int(input())*2 + int(input())*1
+b = int(input())*3 + int(input())*2 + int(input())*1
+if a > b:
+    print("A")
+elif a < b:
+    print("B")
+else:
+    print("T")
