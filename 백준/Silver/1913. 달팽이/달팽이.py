005edbf5b66bf54af503b638eafd0f7a476a3218
Minsoo Jun <95160680+line1029@users.noreply.github.com>
2024-07-02 21:33:00 +0900

    [Silver III] Title: 달팽이, Time: 168 ms, Memory: 138744 KB -BaekjoonHub

--- 백준/Silver/1913. 달팽이/달팽이.py
+++ 백준/Silver/1913. 달팽이/달팽이.py
@@ -1,37 +1,32 @@
 from sys import stdin, stdout
 
-
-def main(n, k):
-    d = [[-1, 0], [0, 1], [1, 0], [0, -1]]
-    grid = [[0] * n for _ in range(n)]
-    x = y = n >> 1
-    grid[x][y] = cur = 1
-    ans = [x + 1, y + 1]
-    idx = 0
-    for level in range(1, n + 1):
-        dx, dy = d[idx]
-        for i in range(level):
-            x += dx
-            y += dy
-            cur += 1
-            grid[x][y] = cur
-            if k == cur:
-                ans = [x + 1, y + 1]
-            if cur == n * n and n == i + 2:
-                stdout.write("\n".join(" ".join(map(str, row)) for row in grid))
-                stdout.write(f"\n{ans[0]} {ans[1]}")
-                return
-        idx = (idx + 1) % 4
-        dx, dy = d[idx]
-        for i in range(level):
-            x += dx
-            y += dy
-            cur += 1
-            grid[x][y] = cur
-            if k == cur:
-                ans = [x + 1, y + 1]
-        idx = (idx + 1) % 4
-
-
-if __name__ == "__main__":
-    main(*map(int, stdin.readlines()))
+n, k = map(int, stdin.readlines())
+d = [[-1, 0], [0, 1], [1, 0], [0, -1]]
+grid = [[0] * n for _ in range(n)]
+x = y = n >> 1
+grid[x][y] = cur = 1
+ans = [x + 1, y + 1]
+idx = 0
+for level in range(1, n + 1):
+    dx, dy = d[idx]
+    for i in range(level):
+        x += dx
+        y += dy
+        cur += 1
+        grid[x][y] = cur
+        if k == cur:
+            ans = [x + 1, y + 1]
+        if cur == n * n and n == i + 2:
+            stdout.write("\n".join(" ".join(map(str, row)) for row in grid))
+            stdout.write(f"\n{ans[0]} {ans[1]}")
+            exit()
+    idx = (idx + 1) % 4
+    dx, dy = d[idx]
+    for i in range(level):
+        x += dx
+        y += dy
+        cur += 1
+        grid[x][y] = cur
+        if k == cur:
+            ans = [x + 1, y + 1]
+    idx = (idx + 1) % 4
