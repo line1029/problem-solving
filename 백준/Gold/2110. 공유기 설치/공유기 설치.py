5e50d53b9c247675ff6ef2871c22726c29de96fa
Minsoo Jun <95160680+line1029@users.noreply.github.com>
2024-07-16 20:36:10 +0900

    [Gold IV] Title: 공유기 설치, Time: 148 ms, Memory: 52724 KB -BaekjoonHub

--- 백준/Gold/2110. 공유기 설치/공유기 설치.py
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.py
@@ -1,18 +1,23 @@
 from sys import stdin, stdout
 
-n, c = map(int, stdin.readline().split())
-houses = sorted(map(int, stdin.readlines()))
-lo, hi = 1, (houses[-1] - houses[0]) // (c - 1)
-while lo < hi:
-    mid = (lo + hi + 1) >> 1
-    cnt, prev = 1, houses[0]
-    for i in range(1, n):
-        if houses[i] - prev >= mid:
-            prev = houses[i]
-            cnt += 1
-        if cnt == c:
-            lo = mid
-            break
-    else:
-        hi = mid - 1
-stdout.write(str(lo))
+
+def main():
+    n, c = map(int, stdin.readline().split())
+    houses = sorted(map(int, stdin.readlines()))
+    lo, hi = 1, (houses[-1] - houses[0]) // (c - 1)
+    while lo < hi:
+        mid = (lo + hi + 1) >> 1
+        cnt, prev = 1, houses[0]
+        for i in range(1, n):
+            if houses[i] - prev >= mid:
+                prev = houses[i]
+                cnt += 1
+            if cnt == c:
+                lo = mid
+                break
+        else:
+            hi = mid - 1
+    stdout.write(str(lo))
+
+
+main()
