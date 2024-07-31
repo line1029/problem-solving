d80fab02a02d91f3abf75bc3ba71065847bd61fa
Minsoo Jun <95160680+line1029@users.noreply.github.com>
2024-07-31 17:59:54 +0900

    Time: 193 ms (59.77%), Space: 31.7 MB (44.46%) - LeetHub

--- LeetCode/Medium/1352-product-of-the-last-k-numbers/1352-product-of-the-last-k-numbers.py
+++ LeetCode/Medium/1352-product-of-the-last-k-numbers/1352-product-of-the-last-k-numbers.py
@@ -1,23 +1,20 @@
 class ProductOfNumbers:
 
     def __init__(self):
-        self.arr = []
-        self.prefix_product = [1]
-        self.last_zero_idx = None
+        self.prod = [1]
+        
 
     def add(self, num: int) -> None:
-        self.arr.append(num)
-        num_product = self.prefix_product[-1]
         if num:
-            num_product *= num
+            self.prod.append(self.prod[-1] * num)
         else:
-            self.last_zero_idx = len(self.arr) - 1
-        self.prefix_product.append(num_product)
+            self.prod = [1]
+        
 
     def getProduct(self, k: int) -> int:
-        if self.last_zero_idx is not None and len(self.arr) - self.last_zero_idx <= k:
+        if k >= len(self.prod):
             return 0
-        return self.prefix_product[-1] // self.prefix_product[-k-1]
+        return self.prod[-1] // self.prod[-k - 1]       
 
 
 # Your ProductOfNumbers object will be instantiated and called as such:
