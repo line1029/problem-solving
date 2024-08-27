4e139ec02c87d350f6472d7e644ebb69e27355f2
Minsoo Jun <95160680+line1029@users.noreply.github.com>
2024-08-27 20:05:05 +0900

    [Silver I] Title: 신입 사원, Time: 580 ms, Memory: 126612 KB -BaekjoonHub

--- 백준/Silver/1946. 신입 사원/신입 사원.py
+++ 백준/Silver/1946. 신입 사원/신입 사원.py
@@ -2,15 +2,18 @@ from sys import stdin, stdout
 
 res = []
 for _ in range(int(stdin.readline())):
-    applies = sorted(
-        map(lambda x: list(map(int, x.split())), (stdin.readline() for __ in range(int(stdin.readline()))))
-    )
-    cur = applies[0][1]
+    n = int(stdin.readline())
+    applies = [0] * n
+    for __ in range(n):
+        i, j = map(int, stdin.readline().split())
+        applies[i - 1] = j
+    cur = applies[0]
     ans = 1
-    for i, j in applies:
+    for j in applies:
         if j < cur:
             ans += 1
             cur = j
-        if cur == 1: break
+        if cur == 1:
+            break
     res.append(ans)
 stdout.write("\n".join(map(str, res)))
