5c2529db7ae15a73d994eab1cd3c3faaa2bfb332
Minsoo Jun <95160680+line1029@users.noreply.github.com>
2024-08-27 19:56:33 +0900

    [Silver I] Title: 신입 사원, Time: 4152 ms, Memory: 217444 KB -BaekjoonHub

---
+++ 백준/Silver/1946. 신입 사원/신입 사원.py
@@ -0,0 +1,15 @@
+from sys import stdin, stdout
+
+res = []
+for _ in range(int(stdin.readline())):
+    applies = sorted(
+        map(lambda x: list(map(int, x.split())), (stdin.readline() for __ in range(int(stdin.readline()))))
+    )
+    cur = applies[0][1]
+    ans = 1
+    for i, j in applies:
+        if j < cur:
+            ans += 1
+            cur = j
+    res.append(ans)
+stdout.write("\n".join(map(str, res)))
