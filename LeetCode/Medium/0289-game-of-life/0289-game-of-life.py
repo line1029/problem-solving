2046db90bca2922c309011ad1e66dbc2cffda168
Minsoo Jun <95160680+line1029@users.noreply.github.com>
2024-10-14 21:38:42 +0900

    Time: 41 ms (25.22%), Space: 16.5 MB (65.73%) - LeetHub

--- LeetCode/Medium/0289-game-of-life/0289-game-of-life.py
+++ LeetCode/Medium/0289-game-of-life/0289-game-of-life.py
@@ -2,14 +2,16 @@ class Solution:
     def gameOfLife(self, board: List[List[int]]) -> None:
         m, n = len(board), len(board[0])
         D = [[-1, 0], [-1, -1], [0, -1], [1, -1], [1, 0], [1, 1], [0, 1], [-1, 1]]
+        l1 = [1, 3]
+        l2 = [2, 3]
         for i in range(m):
             for j in range(n):
                 live_neis = 0
                 for di, dj in D:
                     ni, nj = i + di, j + dj
-                    if 0 <= ni < m and 0 <= nj < n and board[ni][nj] in [1, 3]:
+                    if 0 <= ni < m and 0 <= nj < n and board[ni][nj] in l1:
                         live_neis += 1
-                if board[i][j] and live_neis not in [2, 3] or not board[i][j] and live_neis == 3:
+                if board[i][j] and live_neis not in l2 or not board[i][j] and live_neis == 3:
                     board[i][j] += 2
         for i in range(m):
             for j in range(n):
