35c7f453ed2bd8e95d20a0676672cc99773945ac
Minsoo Jun <95160680+line1029@users.noreply.github.com>
2024-11-18 21:39:52 +0900

    [Gold III] Title: ABBC, Time: 172 ms, Memory: 43548 KB, Score: 5 point -BaekjoonHub

---
+++ 백준/Gold/25381. ABBC/ABBC.py
@@ -0,0 +1,28 @@
+from sys import stdin
+from collections import deque
+
+
+def main():
+    s = stdin.readline()
+    n = len(s)
+    if s[-1] == "\n":
+        n -= 1
+    a = deque(filter(lambda x: s[x] == "A", range(n)))
+    b = deque(filter(lambda x: s[x] == "B", range(n)))
+    c = deque(filter(lambda x: s[x] == "C", range(n)))
+    ans = 0
+    for idx_c in c:
+        if not b or b[0] > idx_c:
+            break
+        b.popleft()
+        ans += 1
+    for idx_a in reversed(a):
+        if not b or b[-1] < idx_a:
+            continue
+        b.pop()
+        ans += 1
+
+    print(ans)
+
+
+main()
